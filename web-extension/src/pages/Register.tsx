151fd044e800a9c7e1f408d50fb69deeaba0cbac
Petr <petrik.spac@gmail.com>
2021-06-30 15:33:21 +0200

    Register onClick

--- web-extension/src/pages/Register.tsx
+++ web-extension/src/pages/Register.tsx
@@ -12,7 +12,8 @@ import {
   Input,
   InputGroup,
   InputRightElement,
-  Text
+  Text,
+  Spinner
 } from '@chakra-ui/react'
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
 import { useRegisterMutation } from './Register.codegen'
@@ -29,114 +30,19 @@ export default function Register(): ReactElement {
   const [register, { data, loading, error: registerError }] =
     useRegisterMutation()
 
-  // if (loading) {
-  //   return (
-  //     <Box>
-  //       <Text>Loading</Text>
-  //     </Box>
-  //   )
-  // }
-
   return (
-    // <Flex width="full" align="center" justifyContent="center">
-    //   <Box
-    //     p={8}
-    //     maxWidth="500px"
-    //     borderWidth={1}
-    //     borderRadius={8}
-    //     boxShadow="lg"
-    //   >
-    //     {isLoggedIn ? (
-    //       <Box textAlign="center">
-    //         <Text>{email} logged in!</Text>
-    //         <Button
-    //           variantColor="orange"
-    //           variant="outline"
-    //           width="full"
-    //           mt={4}
-    //           onClick={() => setIsLoggedIn(false)}
-    //         >
-    //           Sign out
-    //         </Button>
-    //       </Box>
-    //     ) : (
-    //       <>
-    //         <Box textAlign="center">
-    //           <Heading>Login</Heading>
-    //         </Box>
-    //         <Box my={4} textAlign="left">
-    //           <form onSubmit={handleSubmit}>
-    //             {error && <ErrorMessage message={error} />}
-    //             <FormControl isRequired>
-    //               <FormLabel>Email</FormLabel>
-    //               <Input
-    //                 type="email"
-    //                 placeholder="test@test.com"
-    //                 size="lg"
-    //                 onChange={(event) => setEmail(event.currentTarget.value)}
-    //               />
-    //             </FormControl>
-    //             <FormControl isRequired mt={6}>
-    //               <FormLabel>Password</FormLabel>
-    //               <InputGroup>
-    //                 <Input
-    //                   type={showPassword ? 'text' : 'password'}
-    //                   placeholder="*******"
-    //                   size="lg"
-    //                   onChange={(event) =>
-    //                     setPassword(event.currentTarget.value)
-    //                   }
-    //                 />
-    //                 <InputRightElement width="3rem">
-    //                   <Button
-    //                     h="1.5rem"
-    //                     size="sm"
-    //                     onClick={handlePasswordVisibility}
-    //                   >
-    //                     {showPassword ? (
-    //                       <Icon name="view-off" />
-    //                     ) : (
-    //                       <Icon name="view" />
-    //                     )}
-    //                   </Button>
-    //                 </InputRightElement>
-    //               </InputGroup>
-    //             </FormControl>
-    //             <Button
-    //               variantColor="teal"
-    //               variant="outline"
-    //               type="submit"
-    //               width="full"
-    //               mt={4}
-    //             >
-    //               {isLoading ? (
-    //                 <CircularProgress
-    //                   isIndeterminate
-    //                   size="24px"
-    //                   color="teal"
-    //                 />
-    //               ) : (
-    //                 'Sign In'
-    //               )}
-    //             </Button>
-    //           </form>
-    //         </Box>
-    //       </>
-    //     )}
-    //   </Box>
-    // </Flex>
-
     <Box p={8} maxWidth="500px" borderWidth={1} borderRadius={6} boxShadow="lg">
       <Formik
         initialValues={{ email: '', password: '' }}
-        onSubmit={(
+        onSubmit={async (
           values: Values,
           { setSubmitting }: FormikHelpers<Values>
         ) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2))
-            setSubmitting(false)
-          }, 1000)
+          const response = await register({
+            variables: { email: values.email, password: values.password }
+          })
+          setSubmitting(false)
+          console.log(response)
         }}
       >
         {(props) => (
