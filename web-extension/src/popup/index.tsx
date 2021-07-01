b899280f021daaf39e511fcd488d9c79370309c5
Petr <petrik.spac@gmail.com>
2021-07-01 14:38:26 +0200

    store accessToken between refreshes

--- web-extension/src/popup/index.tsx
+++ web-extension/src/popup/index.tsx
@@ -2,17 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { browser } from 'webextension-polyfill-ts'
 import { Popup } from './Popup'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { getAccessToken } from '@src/util/accessToken'
+import App from './App'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:5050/graphql'
+})
+
+const authLink = setContext((_, { headers }) => {
+  //get the authentication token
+  const accessToken = getAccessToken()
+  //return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: accessToken ? `bearer ${accessToken}` : ''
+    }
+  }
+})
 
 const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  uri: 'http://localhost:5050/graphql'
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
 })
 
 browser.tabs.query({ active: true, currentWindow: true }).then(() => {
   ReactDOM.render(
     <ApolloProvider client={client}>
-      <Popup />
+      <App />
     </ApolloProvider>,
     document.getElementById('popup')
   )
