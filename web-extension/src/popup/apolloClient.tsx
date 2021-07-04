6b73ffd4710a973a1cc0c75eae3d01380a5143cb
Jiří Špác <capajj@gmail.com>
2021-07-04 04:21:46 +0200

    add copy and show/hide to OtpCode component
    Former-commit-id: 96825fd73765b769f8baaedcea975d44757b89bb

---
+++ web-extension/src/popup/apolloClient.tsx
@@ -0,0 +1,76 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  ApolloLink
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { getAccessToken, setAccessToken } from '@src/util/accessToken'
+import { onError } from '@apollo/client/link/error'
+import { TokenRefreshLink } from 'apollo-link-token-refresh'
+import jwtDecode from 'jwt-decode'
+
+const apiUrl = 'http://localhost:5050'
+const httpLink = createHttpLink({
+  uri: `${apiUrl}/graphql`
+})
+const tokenRefresh = new TokenRefreshLink({
+  accessTokenField: 'accessToken',
+  isTokenValidOrUndefined: () => {
+    const token = getAccessToken()
+
+    if (!token) {
+      return true
+    }
+
+    try {
+      // @ts-expect-error
+      const { exp } = jwtDecode(token)
+      if (Date.now() >= exp * 1000) {
+        return false
+      } else {
+        return true
+      }
+    } catch (error) {
+      return false
+    }
+  },
+  fetchAccessToken: async () => {
+    return await fetch(`${apiUrl}/refresh_token`, {
+      method: 'POST',
+      credentials: 'include'
+    })
+  },
+  handleFetch: (accessToken) => {
+    setAccessToken(accessToken)
+  },
+  handleError: (err) => {
+    console.warn('Your refresh token is invalid. Try to login again')
+    console.error(err)
+  }
+})
+// Log any GraphQL errors or network error that occurred
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    )
+  if (networkError) console.log(`[Network error]: ${networkError}`)
+})
+const authLink = setContext((_, { headers }) => {
+  //get the authentication token
+  const accessToken = getAccessToken()
+  //return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: accessToken ? `bearer ${accessToken}` : ''
+    }
+  }
+})
+export const apolloClient = new ApolloClient({
+  link: ApolloLink.from([errorLink, tokenRefresh, authLink, httpLink]),
+  cache: new InMemoryCache()
+})
