65a2647d82164bc389a5415a3375a7d897ec1f98
Jiří Špác <capajj@gmail.com>
2021-07-04 04:21:46 +0200

    add copy and show/hide to OtpCode component

--- web-extension/src/popup/AuthsList.tsx
+++ web-extension/src/popup/AuthsList.tsx
@@ -1,36 +1,79 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import {
   Avatar,
   Box,
+  Button,
   Flex,
   Stat,
   StatLabel,
-  StatNumber
+  StatNumber,
+  useClipboard
 } from '@chakra-ui/react'
 import { authenticator } from 'otplib'
-import { AuthsContext } from './Popup'
+import { AuthsContext, IAuth } from './Popup'
+import { CopyIcon, ViewIcon } from '@chakra-ui/icons'
+import { Tooltip } from '@chakra-ui/react'
+import { t } from '@lingui/macro'
 
-export const AuthsList: React.FC<{}> = () => {
+const OtpCode = ({ auth }: { auth: IAuth }) => {
+  const otpCode = authenticator.generate(auth.secret)
+
+  useEffect(() => {
+    setShowWhole(false)
+  }, [otpCode])
+  const [showWhole, setShowWhole] = useState(false)
+  const { onCopy } = useClipboard(otpCode)
+  return (
+    <Box boxShadow="2xl" p="4" rounded="md" bg="white">
+      <Stat>
+        <Flex justify="flex-start" align="center">
+          <Avatar src={auth.icon} size="sm"></Avatar>
+          <Box ml={4} mr="auto">
+            <StatLabel>{auth.label}</StatLabel>
+
+            <StatNumber
+              onClick={() => {
+                setShowWhole(!showWhole)
+                if (!showWhole) {
+                  // TODO log usage of this key to backend
+                }
+              }}
+            >
+              {showWhole ? (
+                otpCode
+              ) : (
+                <Tooltip label={t`Click to show the whole`}>
+                  {otpCode.substr(0, 3) + '***'}
+                </Tooltip>
+              )}
+            </StatNumber>
+          </Box>
+
+          <Button
+            size="md"
+            ml={2}
+            variant="outline"
+            onClick={() => {
+              onCopy()
+
+              // TODO log usage of this token to backend
+            }}
+          >
+            <CopyIcon></CopyIcon>
+          </Button>
+        </Flex>
+      </Stat>
+    </Box>
+  )
+}
+
+export const AuthsList = () => {
   const { auths } = useContext(AuthsContext)
 
   return (
     <>
-      {auths.map((oauth) => {
-        return (
-          <Box boxShadow="2xl" p="4" rounded="md" bg="white">
-            <Stat>
-              <Flex justify="flex-start">
-                <Avatar src={oauth.icon}></Avatar>
-                <Box ml={4}>
-                  <StatLabel>{oauth.label}</StatLabel>
-                  <StatNumber>
-                    {authenticator.generate(oauth.secret)}
-                  </StatNumber>
-                </Box>
-              </Flex>
-            </Stat>
-          </Box>
-        )
+      {auths.map((auth, i) => {
+        return <OtpCode auth={auth} key={auth.label + i} />
       })}
     </>
   )
