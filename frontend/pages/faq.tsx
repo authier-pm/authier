684bf0ddb8f5c26a68220946655959fc5427acb7
Jiří Špác <capajj@gmail.com>
2021-07-06 04:38:39 +0200

    add a bit of FAQ

--- frontend/pages/faq.tsx
+++ frontend/pages/faq.tsx
@@ -1,14 +1,31 @@
+import { Box, Heading } from '@chakra-ui/react'
 import Head from 'next/head'
 import React from 'react'
 import { PageHeading } from '../components/PageHeading'
 
 export default function Pricing() {
   return (
-    <>
+    <Box m={5}>
       <Head>
         <title>Authier - FAQ</title>
       </Head>
       <PageHeading> FAQ</PageHeading>
-    </>
+
+      <ul>
+        <li>
+          <Heading size="xs">
+            Doesn't it defeat the purpose of 2FA when you have codes available
+            on the same device?
+          </Heading>
+          <br />
+          No not really. Projects like Authy already allow this for number of
+          years. We go further than them to ensure user's security. We lock your
+          codes whenever user wishes-so for a security minded configuration of
+          Authier you are still required to use your mobile phone. Even when you
+          have browser extension installed. Browser extension just takes care of
+          the typing for you once you use biometrics to unlock your vault.
+        </li>
+      </ul>
+    </Box>
   )
 }
