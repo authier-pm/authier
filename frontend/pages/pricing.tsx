5e2e15eff6daee086f8060fd52c47d65721bf932
Jiří Špác <capajj@gmail.com>
2021-07-06 04:05:02 +0200

    a bit of styles

--- frontend/pages/pricing.tsx
+++ frontend/pages/pricing.tsx
@@ -1,14 +1,17 @@
-import { Heading } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import Head from 'next/head'
 import React from 'react'
+import { PageHeading } from '../components/PageHeading'
 
 export default function Pricing() {
   return (
     <>
-      <Head>
-        <title>Authier - Pricing</title>
-      </Head>
-      <Heading> Pricing</Heading>
+      <Box bgGradient="linear(to-l, teal.100, teal.400)" minH="90vh">
+        <Head>
+          <title>Authier - Pricing</title>
+        </Head>
+        <PageHeading>Pricing</PageHeading>
+      </Box>
     </>
   )
 }
