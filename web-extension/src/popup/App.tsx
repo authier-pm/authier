3b2a9dfa6ad422143607da4d213fdc291e189ccf
Petr <petrik.spac@gmail.com>
2021-07-06 22:25:43 +0200

    Check accessToken on Popup

--- web-extension/src/popup/App.tsx
+++ web-extension/src/popup/App.tsx
@@ -1,15 +1,14 @@
 import { Flex } from '@chakra-ui/react'
 import { setAccessToken } from '@src/util/accessToken'
 import React, { ReactElement, useEffect, useState } from 'react'
+import { browser } from 'webextension-polyfill-ts'
 import { Popup } from './Popup'
-
-interface Props {}
 
 function App(): ReactElement {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    fetch('http://localhost:5050/refresh_token', {
+    fetch('http://localhost:5051/refresh_token', {
       method: 'POST',
       credentials: 'include'
     }).then(async (x) => {
@@ -27,3 +26,19 @@ function App(): ReactElement {
 }
 
 export default App
+
+// fetch('http://localhost:5051/refresh_token', {
+//   method: 'POST',
+//   credentials: 'include'
+// }).then(async (x) => {
+//   const { accessToken } = await x.json()
+//   setAccessToken(accessToken)
+//   setLoading(false)
+// })
+
+// async function token() {
+//   let s = await browser.storage.local.get('jid')
+//   setAccessToken(s.jid)
+//   setLoading(false)
+// }
+// token()
