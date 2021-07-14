50d456f2d36f222ffc26baae8b7f9e7e70535b1d
Petr <petrik.spac@gmail.com>
2021-07-14 11:36:55 +0200

    Renamed with react-native-rename on "authier"

--- mobileApp/ios/authierTests/authierTests.m
+++ mobileApp/ios/authierTests/authierTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface mobileAppTests : XCTestCase
+@interface authierTests : XCTestCase
 
 @end
 
-@implementation mobileAppTests
+@implementation authierTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
