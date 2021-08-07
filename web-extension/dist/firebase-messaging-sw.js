217cfea62e7219bc3a03f42c2f63ac1c4354d409
Petr <petrik.spac@gmail.com>
2021-08-07 11:08:36 +0200

    trying to add service worker

--- web-extension/dist/firebase-messaging-sw.js
+++ web-extension/dist/firebase-messaging-sw.js
@@ -1,18 +1,27 @@
-import { getMessaging } from 'firebase/messaging'
-import { onBackgroundMessage } from 'firebase/messaging/sw'
+// Scripts for firebase and firebase messaging
+importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js')
 
-const messaging = getMessaging()
-onBackgroundMessage(messaging, (payload) => {
-  console.log(
-    '[firebase-messaging-sw.js] Received background message ',
-    payload
-  )
-  // Customize notification here
-  const notificationTitle = 'Background Message Title'
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  }
+// Initialize the Firebase app in the service worker by passing the generated config
+var firebaseConfig = {
+  apiKey: 'AIzaSyBkBIcE71acyLg1yMNJwn3Ys_CxbY5gt7U',
+  authDomain: 'authier-bc184.firebaseapp.com',
+  projectId: 'authier-bc184',
+  storageBucket: 'authier-bc184.appspot.com',
+  messagingSenderId: '500382892914',
+  appId: '1:500382892914:web:6b202f90d6c0c6bcc213eb',
+  measurementId: 'G-0W2MW55WVF'
+}
 
-  self.registration.showNotification(notificationTitle, notificationOptions)
+firebase.initializeApp(firebaseConfig)
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging()
+
+messaging.onBackgroundMessage(function (payload) {
+  console.log('Received background message ', payload)
+
+  self.registration.showNotification('test', {
+    body: payload.data.success
+  })
 })
