addaa92bede86d2ce7f9103ebcac669771d13bde
Rambo <petrik.spac@gmail.com>
2021-08-20 16:31:16 +0200

    work in progress

--- web-extension/src/util/useBackground.tsx
+++ web-extension/src/util/useBackground.tsx
@@ -1,4 +1,5 @@
 import { MessageType, sharedBrowserEvents } from '@src/backgroundPage'
+import { Passwords } from '@src/providers/PasswProvider'
 import { useState, useEffect } from 'react'
 import { browser } from 'webextension-polyfill-ts'
 
@@ -17,11 +18,14 @@ export function useBackground() {
   const [bgAuths, setBgAuths] = useState<IAuth[] | undefined>(undefined)
   const [isFilling, setIsFilling] = useState<Boolean>(false)
   const [isCounting, setIsCounting] = useState<Boolean>(false)
+  const [bgPasswords, setBgPasswords] = useState<Passwords[] | undefined>(
+    undefined
+  )
 
   useEffect(() => {
     //Get auth from bg
     chrome.runtime.sendMessage(
-      MessageType.giveMeAuths,
+      { action: MessageType.giveMeAuths },
       function (res: { auths: Array<IAuth> }) {
         if (res && res.auths) {
           setBgAuths(res.auths)
@@ -29,8 +33,18 @@ export function useBackground() {
       }
     )
 
+    //Get passwords from bg
     chrome.runtime.sendMessage(
-      { wasClosed: true },
+      { action: MessageType.giveMePasswords },
+      function (res: { passwords: Array<Passwords> }) {
+        if (res && res.passwords) {
+          setBgPasswords(res.passwords)
+        }
+      }
+    )
+
+    chrome.runtime.sendMessage(
+      { action: MessageType.wasClosed },
       (res: { wasClosed: Boolean }) => {
         if (res.wasClosed) {
           setSafeLocked(true)
@@ -73,13 +87,27 @@ export function useBackground() {
     isFilling,
     setSafeLockTime: async (lockTime: number | null) => {
       chrome.runtime.sendMessage({
+        action: MessageType.lockTime,
         lockTime: lockTime
       })
       setSafeLockTime(lockTime)
     },
     safeLockTime,
+    savePasswodsToBg: (value: Passwords[] | undefined) => {
+      chrome.runtime.sendMessage({
+        passwords: value
+      })
+
+      //@ts-expect-error
+      if (value?.length > 0) {
+        setBgPasswords(value)
+      } else {
+        setBgPasswords([])
+      }
+    },
     saveAuthsToBg: (value: IAuth[] | undefined) => {
       chrome.runtime.sendMessage({
+        action: MessageType.auths,
         auths: value
       })
       //@ts-expect-error
@@ -92,7 +120,7 @@ export function useBackground() {
     bgAuths,
     startCount: () => {
       chrome.runtime.sendMessage(
-        { startCount: true },
+        { action: MessageType.startCount },
         (res: { isCounting: Boolean }) => {
           if (res.isCounting) {
             setIsCounting(true)
