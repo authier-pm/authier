2c4d7f815f31f653d93a0dc32b48b4eb350216fc
sleaper <petrik.spac@gmail.com>
2021-08-27 19:13:42 +0200

    added UI section to settings
    Former-commit-id: 3587af599633970f3ad145cedd3efb90d101a7af

--- web-extension/src/pages/Settings.tsx
+++ web-extension/src/pages/Settings.tsx
@@ -2,101 +2,46 @@ import { Flex } from '@chakra-ui/layout'
 import {
   Text,
   Button,
-  Checkbox,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Heading,
-  Input,
-  Select,
-  StackDivider,
-  VStack
+  Box,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Image
 } from '@chakra-ui/react'
 import { Formik, Form, Field, FormikHelpers } from 'formik'
-import React from 'react'
+import React, { useState } from 'react'
 import { useBackground } from '@src/util/useBackground'
-
-export interface Config {
-  vaultTime: string
-  noHandsLogin: boolean
-}
+import { ChevronDownIcon } from '@chakra-ui/icons'
+import Security from '@src/components/setting-screens/Security'
+import UI from '@src/components/setting-screens/UI'
 
 export const Settings = () => {
-  const { setSettingsConfig } = useBackground()
+  const [currSett, setCurrSett] = useState<'Security' | 'UI'>('Security')
 
   return (
     <Flex flexDirection="column" m={5}>
-      <Heading>Security</Heading>
-      <Formik
-        initialValues={{
-          vaultTime: '12 hours',
-          noHandsLogin: false
-        }}
-        onSubmit={async (
-          values: Config,
-          { setSubmitting }: FormikHelpers<Config>
-        ) => {
-          alert(JSON.stringify(values, null, 2))
-          setSettingsConfig({
-            vaultTime: values.vaultTime,
-            noHandsLogin: values.noHandsLogin
-          })
+      <Menu>
+        <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+          {currSett}
+        </MenuButton>
+        <MenuList>
+          <MenuItem minH="48px" onClick={() => setCurrSett('Security')}>
+            <Text fontSize="md">Security</Text>
+          </MenuItem>
+          <MenuItem minH="40px" onClick={() => setCurrSett('UI')}>
+            <Text fontSize="md">UI</Text>
+          </MenuItem>
+        </MenuList>
+      </Menu>
 
-          setSubmitting(false)
-        }}
-      >
-        {(props) => (
-          <Form>
-            <Field name="vaultTime">
-              {/* @ts-expect-error */}
-              {({ field, form }) => (
-                <FormControl
-                  isInvalid={form.errors.vaultTime && form.touched.vaultTime}
-                >
-                  <FormLabel htmlFor="vaultTime">Safe lock time</FormLabel>
-                  <Select
-                    {...field}
-                    id="vaultTime"
-                    placeholder="Select country"
-                    mb={3}
-                  >
-                    <option>On web close</option>
-                    <option>10 secconds</option>
-                    <option>8 hours</option>
-                    <option>12 hours</option>
-                  </Select>
-                  <FormErrorMessage>{form.errors.vaultTime}</FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Field name="noHandsLogin">
-              {/* @ts-expect-error */}
-              {({ field, form }) => (
-                <FormControl
-                  isInvalid={
-                    form.errors.noHandsLogin && form.touched.noHandsLogin
-                  }
-                >
-                  <Checkbox {...field} id="noHandsLogin">
-                    No Hands login
-                  </Checkbox>
-                  <FormErrorMessage>
-                    {form.errors.noHandsLogin}
-                  </FormErrorMessage>
-                </FormControl>
-              )}
-            </Field>
-            <Button
-              mt={4}
-              colorScheme="teal"
-              isLoading={props.isSubmitting}
-              type="submit"
-            >
-              Save
-            </Button>
-          </Form>
-        )}
-      </Formik>
+      <Box mt={2}>
+        {currSett === 'Security' ? (
+          <Security />
+        ) : currSett === 'UI' ? (
+          <UI />
+        ) : null}
+      </Box>
     </Flex>
   )
 }
