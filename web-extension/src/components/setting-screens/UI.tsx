4d7aa0398b562e0d73956e055c56bb72e795574f
Rambo <petrik.spac@gmail.com>
2021-08-28 10:16:44 +0200

    Added react-select

--- web-extension/src/components/setting-screens/UI.tsx
+++ web-extension/src/components/setting-screens/UI.tsx
@@ -1,7 +1,6 @@
 import {
   FormControl,
   FormLabel,
-  Select,
   FormErrorMessage,
   Checkbox,
   Button
@@ -9,12 +8,19 @@ import {
 import { useBackground } from '@src/util/useBackground'
 import { Formik, FormikHelpers, Form, Field } from 'formik'
 import React, { ReactElement } from 'react'
+import Select from 'react-select'
 
 interface Props {}
 
 export interface UISettings {
   homeList: 'All' | 'TOTP & Login credencials' | 'Current domain'
 }
+
+const options = [
+  { value: 'All', label: 'All' },
+  { value: 'TOTP & Login credencials', label: 'TOTP & Login credencials' },
+  { value: 'Current domain', label: 'Current domain' }
+]
 
 export default function UI({}: Props): ReactElement {
   const { setUISettings, UIConfig } = useBackground()
@@ -41,11 +47,23 @@ export default function UI({}: Props): ReactElement {
                   isInvalid={form.errors.homeList && form.touched.homeList}
                 >
                   <FormLabel htmlFor="homeList">Home list</FormLabel>
-                  <Select {...field} id="homeList" mb={3}>
-                    <option>All</option>
-                    <option> TOTP & Login credencials</option>
-                    <option>Current domain</option>
-                  </Select>
+                  <Select
+                    options={options}
+                    name={field.name}
+                    //@ts-expect-error
+                    value={
+                      options
+                        ? options.find((option) => option.value === field.value)
+                        : ''
+                    }
+                    onChange={(option) =>
+                      //@ts-expect-error
+                      form.setFieldValue(field.name, option.value)
+                    }
+                    onBlur={field.onBlur}
+                    id="homeList"
+                    mb={3}
+                  />
                   <FormErrorMessage>{form.errors.homeList}</FormErrorMessage>
                 </FormControl>
               )}
