7af5126820b7d65c1972ba0232f0432c4b850059
Jiří Špác <capajj@gmail.com>
2021-08-30 13:36:16 +0200

    fix bitfinex TOTP adding
    Former-commit-id: a7af3008beb276803c7fe01b203c1e708e6c5a19

--- web-extension/src/components/AddAuthSecretButton.spec.tsx
+++ web-extension/src/components/AddAuthSecretButton.spec.tsx
@@ -1,3 +1,45 @@
+import { getTokenSecretFromQrCode } from './AddAuthSecretButton'
+
 describe('getTokenSecretFromQrCode', () => {
-  it.todo('should work for all types of TOTP secret QR codes on the web')
+  it('should work for bitfinex TOTP secret QR codes with/without issuer', () => {
+    const secretToken = getTokenSecretFromQrCode(
+      {
+        data: 'otpauth://totp/Bitfinex-8-30-2021?secret=SuperDUperSecretToken&issuer=Bitfinex'
+      } as any,
+      {
+        favIconUrl: 'test.ico',
+        title: 'testTitle',
+        url: 'https://www.bitfinex.com/'
+      } as any
+    )
+
+    expect(secretToken).toMatchInlineSnapshot(`
+      Object {
+        "icon": "test.ico",
+        "label": "Bitfinex",
+        "originalUrl": "https://www.bitfinex.com/",
+        "secret": "SuperDUperSecretToken",
+      }
+    `)
+
+    expect(
+      getTokenSecretFromQrCode(
+        {
+          data: 'otpauth://totp/Bitfinex-8-30-2021?secret=SuperDUperSecretToken'
+        } as any,
+        {
+          favIconUrl: 'test.ico',
+          title: 'testTitle',
+          url: 'https://www.bitfinex.com/'
+        } as any
+      )
+    ).toMatchInlineSnapshot(`
+      Object {
+        "icon": "test.ico",
+        "label": "Bitfinex-8-30-2021",
+        "originalUrl": "https://www.bitfinex.com/",
+        "secret": "SuperDUperSecretToken",
+      }
+    `)
+  })
 })
