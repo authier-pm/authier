fb4251e29a69ffbaaf89486448398a024fa7979c
Rambo <petrik.spac@gmail.com>
2021-08-30 11:25:29 +0200

    can remove login or totp

---
+++ web-extension/src/components/RemoveAlertDialog.tsx
@@ -0,0 +1,53 @@
+import { Button } from '@chakra-ui/button'
+import {
+  AlertDialog,
+  AlertDialogOverlay,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogBody,
+  AlertDialogFooter
+} from '@chakra-ui/react'
+import React, { ReactElement } from 'react'
+
+interface Props {
+  isOpen: boolean
+  onClose: any
+  cancelRef: any
+}
+
+export default function RemoveAlertDialog({
+  isOpen,
+  onClose,
+  cancelRef
+}: Props): ReactElement {
+  return (
+    <>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              Delete password
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              Are you sure? You can't undo this action afterwards.
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                Cancel
+              </Button>
+              <Button colorScheme="red" onClick={onClose} ml={3}>
+                Delete
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
+    </>
+  )
+}
