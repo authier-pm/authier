b09c36304a0680e21c915334cff448da51e18b6e
Jiří Špác <capajj@gmail.com>
2021-08-31 19:21:45 +0200

    rename apiUrl constant
    Former-commit-id: c107d02e8e1a6f3d541b8e0cc1463c3ff4330306

--- web-extension/src/apollo/apolloClient.tsx
+++ web-extension/src/apollo/apolloClient.tsx
@@ -15,10 +15,10 @@ import { TokenRefreshLink } from 'apollo-link-token-refresh'
 import jwtDecode from 'jwt-decode'
 import { browser } from 'webextension-polyfill-ts'
 
-const apiUrl = process.env.API_URL
+const API_URL = process.env.API_URL
 
 const httpLink = createHttpLink({
-  uri: apiUrl
+  uri: API_URL
 })
 const tokenRefresh = new TokenRefreshLink({
   accessTokenField: 'accessToken',
@@ -44,7 +44,7 @@ const tokenRefresh = new TokenRefreshLink({
   },
   fetchAccessToken: async () => {
     console.log('refetch JWT token')
-    return await fetch(`http://localhost:5051/refresh_token`, {
+    return await fetch(`${API_URL}/refresh_token`, {
       method: 'POST',
       credentials: 'include'
     })
