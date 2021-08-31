125abca7205853ca8df9a31f221af1b83a9c9822
Jiří Špác <capajj@gmail.com>
2021-09-01 01:19:42 +0200

    plug in react-toastify into frontend too

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,11 @@
 import { ApolloProvider } from '@apollo/client'
 import { useRouter } from 'next/dist/client/router'
 import React from 'react'
+import { toastifyConfig } from '../../shared/toastifyConfig'
 import { apolloClient } from '../graphql/apolloClient'
 import { ChakraLayout } from './layout/ChakraLayout'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
@@ -14,6 +17,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={apolloClient}>
       <ChakraLayout>
+        <ToastContainer {...toastifyConfig('bottom-right')} />
+
         <Component {...pageProps} />
       </ChakraLayout>
     </ApolloProvider>
