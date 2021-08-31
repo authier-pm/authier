635abbf8bdcb5309663a421662943d96858e61a8
Jiří Špác <capajj@gmail.com>
2021-09-01 01:19:42 +0200

    plug in react-toastify into frontend too
    Former-commit-id: 125abca7205853ca8df9a31f221af1b83a9c9822

---
+++ frontend/next.config.js
@@ -0,0 +1,15 @@
+// @ts-check
+const path = require('path')
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  webpack: (config, options) => {
+    config.resolve.alias.react = path.resolve('./node_modules/react') // without this It was throwing "You might have more than one copy of React in the same app"
+
+    return config
+  }
+}
+
+module.exports = nextConfig
---
+++ shared/toastifyConfig.ts
@@ -0,0 +1,13 @@
+import { Flip } from 'react-toastify' // use react-toastify instead of chakra toast. Chakra toast is somehow weirdly broken in extension, see: https://github.com/chakra-ui/chakra-ui/issues/4619
+
+export const toastifyConfig = (position) => {
+  return {
+    limit: 1,
+    closeOnClick: true,
+    pauseOnHover: false,
+    autoClose: 2700,
+    hideProgressBar: true,
+    transition: Flip,
+    position
+  }
+}
