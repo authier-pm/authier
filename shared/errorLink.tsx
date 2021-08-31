d19031350cbc30b4170206be20e488ac9e313778
Jiří Špác <capajj@gmail.com>
2021-09-01 01:19:42 +0200

    plug in react-toastify into frontend too

--- shared/errorLink.tsx
+++ shared/errorLink.tsx
@@ -1,13 +1,23 @@
 import { onError } from '@apollo/client/link/error'
+import { toastifyConfig } from './toastifyConfig'
+import { toast, Theme } from 'react-toastify'
 
 // Log any GraphQL errors or network error that occurred
 export const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors)
+  const isExtension = window.location.href.startsWith('chrome-extension')
+
+  toast.configure(toastifyConfig(isExtension ? undefined : 'bottom-right'))
+  if (graphQLErrors) {
     graphQLErrors.map(({ message, locations, path }) => {
-      console.log(JSON.stringify(locations))
-      console.log(
+      console.error(JSON.stringify(locations))
+      console.error(
         `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
       )
     })
-  if (networkError) console.log(`[Network error]: ${networkError}`)
+    toast.error('There was API error.')
+  }
+  if (networkError) {
+    console.error(`[Network error]: ${networkError}`)
+    toast.error('There was network error.')
+  }
 })
