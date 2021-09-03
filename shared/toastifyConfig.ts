d311e3440aa504bd7844f4416bf4094bb293a4af
Jiří Špác <capajj@gmail.com>
2021-09-03 14:01:44 +0200

    fix TSC
    Former-commit-id: 15f2008165ecdf0a631412cfd3482d1e9eb81fc0

--- shared/toastifyConfig.ts
+++ shared/toastifyConfig.ts
@@ -1,6 +1,6 @@
 import { Flip } from 'react-toastify' // use react-toastify instead of chakra toast. Chakra toast is somehow weirdly broken in extension, see: https://github.com/chakra-ui/chakra-ui/issues/4619
 
-export const toastifyConfig = (position) => {
+export const toastifyConfig = (position: string | undefined) => {
   return {
     limit: 1,
     closeOnClick: true,
