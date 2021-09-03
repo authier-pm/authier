107f33b5a52aa35f36576c3f4741929582640f6f
Jiří Špác <capajj@gmail.com>
2021-09-03 14:22:58 +0200

    fix frontend compilation error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,6 +9,9 @@ const nextConfig = {
     config.resolve.alias.react = path.resolve('./node_modules/react') // without this It was throwing "You might have more than one copy of React in the same app"
 
     return config
+  },
+  experimental: {
+    externalDir: true
   }
 }
 
