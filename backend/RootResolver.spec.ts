bba6c9364f40f03634632a7779fb136d4ac15345
Jiří Špác <capajj@gmail.com>
2021-09-06 09:11:12 +0200

    fix specs

--- backend/RootResolver.spec.ts
+++ backend/RootResolver.spec.ts
@@ -1,11 +1,13 @@
 import { RootResolver } from './RootResolver'
 
 describe('RootResolver', () => {
-  it('should work', async () => {
-    const resolver = new RootResolver()
-    expect(() => {
-      resolver.me({ request: { headers: {} } } as any)
-    }).toThrowErrorMatchingInlineSnapshot(`"You are missing a token"`)
+  describe('me', () => {
+    it('should return null', async () => {
+      const resolver = new RootResolver()
+      expect(
+        await resolver.me({ request: { headers: {} } } as any)
+      ).toBeUndefined()
+    })
   })
 
   describe('logout', () => {
