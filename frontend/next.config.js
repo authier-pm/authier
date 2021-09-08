cba0836ff9033e78242d54f93cee17bbd28e00e2
Jiří Špác <capajj@gmail.com>
2021-09-08 16:03:29 +0200

    fix TSC

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,7 @@ const path = require('path')
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
-  webpack: (config, options) => {
+  webpack: (config, _options) => {
     config.resolve.alias.react = path.resolve('./node_modules/react') // without this It was throwing "You might have more than one copy of React in the same app"
 
     return config
