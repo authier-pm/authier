6f2070b24986a211691c1d265a4d893e33310608
Jiří Špác <capajj@gmail.com>
2021-09-13 23:51:34 +0200

    wip2
    Former-commit-id: fa0d543b1d35874d2b5c30d0e2de6721acc657bf

--- web-extension/src/background/backgroundPage.ts
+++ web-extension/src/background/backgroundPage.ts
@@ -7,7 +7,6 @@ import { twoFAs } from './chromeRuntimeListener'
 import { ILoginCredentials } from '@src/util/useBackgroundState'
 import { noHandsLogin } from './chromeRuntimeListener'
 import { SharedBrowserEvents } from './SharedBrowserEvents'
-//import { initInputWatch } from './initInputWatch'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBkBIcE71acyLg1yMNJwn3Ys_CxbY5gt7U',
@@ -144,96 +143,6 @@ export type SessionStoredItem = {
   label: string
   willSave: boolean
 }
-//const AUTHIER_SESSION_STORAGE_KEY = '__authier'
-
-function initInputWatch(credentials?: string) {
-  const AUTHIER_SESSION_STORAGE_KEY = '__authier'
-  let confirm: boolean
-  let loginFields: any = []
-  let inputs = document.getElementsByTagName('input')
-  for (let j = 0; j < inputs.length; j++) {
-    if (inputs[j].type === 'password') {
-      loginFields = [inputs[j - 1], inputs[j]]
-    }
-  }
-
-  let submit = document.querySelector('#submit') // get the button with some diff way
-
-  let username = loginFields[0]
-  let password = loginFields[1]
-
-  console.log({ credentials, location })
-  console.log(username, password)
-
-  //@ts-expect-error
-  if (username && password && !credentials.hasData) {
-    //@ts-expect-error
-    if (!credentials.username) {
-      let div = document.createElement('div')
-      div.style.height = '42%'
-      div.style.width = '100%'
-      div.style.position = 'fixed'
-      div.style.top = '0px'
-      div.style.left = '0px'
-      div.style.padding = '0px'
-
-      let saveButton = document.createElement('button')
-      saveButton.id = 'save'
-      saveButton.textContent = 'save'
-      saveButton.onclick = function () {
-        confirm = true
-        console.log('save', confirm)
-        div.remove()
-      }
-      div.appendChild(saveButton)
-
-      let closeButton = document.createElement('button')
-      closeButton.id = 'close'
-      closeButton.textContent = 'close'
-      closeButton.onclick = function () {
-        confirm = false
-        console.log('close', confirm)
-        div.remove()
-      }
-      div.appendChild(closeButton)
-
-      document.body.appendChild(div)
-    }
-  }
-
-  document.body.addEventListener('click', (e) => {
-    if (username.value && password.value) {
-      const sessionStoredItem: SessionStoredItem = {
-        username: username.value,
-        password: password.value,
-        originalUrl: location.href,
-        label: location.hostname,
-        willSave: confirm
-      }
-      console.log('test', sessionStoredItem)
-
-      sessionStorage.setItem(
-        AUTHIER_SESSION_STORAGE_KEY,
-        JSON.stringify(sessionStoredItem)
-      )
-    }
-  })
-
-  //@ts-expect-error
-  if (credentials.username) {
-    // TODO fill password & username
-    //@ts-expect-error
-    username.value = credentials.username
-    //@ts-expect-error
-    password.value = credentials.password
-
-    //@ts-expect-error
-    if (credentials.noHandsLogin && submit) {
-      //@ts-expect-error
-      submit.click()
-    }
-  }
-}
 
 function getStoredCredentials() {
   const AUTHIER_SESSION_STORAGE_KEY = '__authier'
@@ -267,15 +176,15 @@ chrome.tabs.onUpdated.addListener(async function (tabId, changeInfo, _tab) {
       return false
     })
 
-    await executeScriptInCurrentTab(
-      `(` +
-        initInputWatch.toString() +
-        `)(${JSON.stringify({
-          ...password,
-          noHandsLogin: noHandsLogin,
-          hasData: !!password
-        })})`
-    )
+    // await executeScriptInCurrentTab(
+    //   `(` +
+    //     initInputWatch.toString() +
+    //     `)(${JSON.stringify({
+    //       ...password,
+    //       noHandsLogin: noHandsLogin,
+    //       hasData: !!password
+    //     })})`
+    // )
 
     if (!password) {
       let scanForItem = setInterval(async () => {
