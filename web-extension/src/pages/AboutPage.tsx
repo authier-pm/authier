afd4cf3ad2e7e10aca2c7c5544499f3d60f9327c
Jiří Špác <capajj@gmail.com>
2021-09-13 09:30:37 +0200

    add a simple about page
    Former-commit-id: d6f99592bda5b78cd28307b11994fb2de5ff24aa

---
+++ web-extension/src/pages/AboutPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import pkg from '../../package.json'
+import { Trans } from '@lingui/macro'
+import { Center, Stack } from '@chakra-ui/layout'
+import { Heading, Text } from '@chakra-ui/react'
+
+export const AboutPage: React.FC<{}> = () => {
+  return (
+    <Center m={2} p={2}>
+      <Stack direction="column">
+        <Heading size="md">Authier web extension</Heading>
+        <Text fontSize={'large'}>
+          <Trans>Version: {pkg.version}</Trans>
+        </Text>
+      </Stack>
+    </Center>
+  )
+}
