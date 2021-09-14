4a345a102dd69c4bd69048d6c445c271339b92f5
Rambo <petrik.spac@gmail.com>
2021-09-14 22:51:23 +0200

    frontend auth iteration

--- frontend/graphql/apolloClient.ts
+++ frontend/graphql/apolloClient.ts
@@ -5,25 +5,12 @@ import {
   InMemoryCache
 } from '@apollo/client'
 import { errorLink } from '../../shared/errorLink'
-
-import { TokenRefreshLink } from 'apollo-link-token-refresh'
 import jwtDecode from 'jwt-decode'
 
-export let accessToken = ''
 export let getTokenFromLocalStorage = async (): Promise<string | null> => {
-  return sessionStorage.getItem('access-token')
-}
-
-export const setAccessToken = (s: string) => {
-  accessToken = s
-}
-
-export const removeToken = () => {
-  accessToken = ''
-}
-
-export const getAccessToken = () => {
-  return accessToken
+  let test = sessionStorage.getItem('access-token')
+  console.log('testik', test)
+  return test
 }
 
 export const getUserFromToken = async () => {
@@ -31,48 +18,10 @@ export const getUserFromToken = async () => {
     (await getTokenFromLocalStorage()) as string
   )
 }
-const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL
-
-export const tokenRefresh = new TokenRefreshLink({
-  accessTokenField: 'accessToken',
-  isTokenValidOrUndefined: () => {
-    //Get token from local storage
-    const token = getAccessToken()
-
-    if (!token) {
-      return true
-    }
-
-    try {
-      // @ts-expect-error
-      const { exp } = jwtDecode(token)
-      if (Date.now() >= exp * 1000) {
-        return false
-      } else {
-        return true
-      }
-    } catch (error) {
-      return false
-    }
-  },
-  fetchAccessToken: async () => {
-    console.log('refetch JWT access token')
-    return await fetch(`${NEXT_PUBLIC_API_URL}/refresh_token`, {
-      method: 'POST',
-      credentials: 'include'
-    })
-  },
-  handleFetch: async (accessToken) => {
-    await sessionStorage.setItem('access-token', accessToken)
-    setAccessToken(accessToken)
-  },
-  handleError: (err) => {
-    console.warn('Your refresh token is invalid. Try to login again', err)
-  }
-})
 
 const httpLink = createHttpLink({
-  uri: `${NEXT_PUBLIC_API_URL}/graphql`
+  uri: `http://localhost:5051/graphql`,
+  credentials: 'include'
 })
 
 export const apolloClient = new ApolloClient({
