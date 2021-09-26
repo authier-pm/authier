15b404348672504f514fb3c47f0adc0709bb16ca
Jiří Špác <capajj@gmail.com>
2021-09-26 19:48:46 +0200

    tweak a few styles and vault filtering

--- web-extension/src/VaultRoutes.tsx
+++ web-extension/src/VaultRoutes.tsx
@@ -1,13 +1,13 @@
 import { Flex, Spinner } from '@chakra-ui/react'
 import React, { ReactElement, useContext } from 'react'
 import AuthPages from './AuthPages'
-import Vault from './pages-vault/Vault'
+import { Vault } from './pages-vault/Vault'
 import { VaultUnlockVerification } from './pages/VaultUnlockVerification'
 import { Popup } from './popup/Popup'
 import { useIsLoggedInQuery } from './popup/Popup.codegen'
 import { BackgroundContext } from './providers/BackgroundProvider'
 
-export default function Routes(): ReactElement {
+export function VaultRoutes(): ReactElement {
   const { masterPassword } = useContext(BackgroundContext)
 
   if (!masterPassword) {
