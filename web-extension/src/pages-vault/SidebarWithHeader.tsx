46af8d71f90960e8f68a20b4be5a4bc77411da8d
sleaper <petrik.spac@gmail.com>
2021-09-26 10:55:12 +0200

    UI tweaks
    Former-commit-id: 85d8cf7d62e2bdb5dd95686c3613e0fe7cee2907

--- web-extension/src/pages-vault/SidebarWithHeader.tsx
+++ web-extension/src/pages-vault/SidebarWithHeader.tsx
@@ -70,8 +70,8 @@ export default function SidebarWithHeader({
         </DrawerContent>
       </Drawer>
       {/* mobilenav */}
-      <MobileNav onOpen={onOpen} />
-      {children}
+      {/* <MobileNav onOpen={onOpen} /> */}
+      <Box ml={['250px']}>{children}</Box>
     </Box>
   )
 }
@@ -98,11 +98,36 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
         </Text>
         <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
       </Flex>
-      {LinkItems.map((link) => (
-        <NavItem key={link.name} icon={link.icon}>
-          {link.name}
-        </NavItem>
-      ))}
+      <Flex flexDirection="column">
+        {LinkItems.map((link) => (
+          <NavItem key={link.name} icon={link.icon}>
+            {link.name}
+          </NavItem>
+        ))}
+      </Flex>
+
+      <Flex>
+        <Avatar
+          size={'sm'}
+          src={
+            'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+          }
+        />
+        <VStack
+          display={{ base: 'none', md: 'flex' }}
+          alignItems="flex-start"
+          spacing="1px"
+          ml="2"
+        >
+          <Text fontSize="sm">Justina Clark</Text>
+          <Text fontSize="xs" color="gray.600">
+            Admin
+          </Text>
+        </VStack>
+        <Box display={{ base: 'none', md: 'flex' }}>
+          <FiChevronDown />
+        </Box>
+      </Flex>
     </Box>
   )
 }
