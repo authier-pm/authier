aee28c231653b505a4246a3d238d8b9fc11042eb
sleaper <petrik.spac@gmail.com>
2021-09-30 18:46:34 +0200

    More routing, ui

--- web-extension/src/pages-vault/Vault.tsx
+++ web-extension/src/pages-vault/Vault.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import SidebarWithHeader from '../components/vault/SidebarWithHeader'
 import { ItemList } from '@src/components/vault/ItemList'
-import { Item } from '@src/components/vault/Item'
+import { ItemSettings } from '@src/pages-vault/ItemSettings'
 import { Route, useLocation, Switch } from 'react-router-dom'
-import { ILoginCredentials } from '@src/util/useBackgroundState'
+import { VaultSettings } from './VaultSettings'
 
 export function Vault() {
   const location = useLocation()
-  console.log(location)
+
   return (
     <SidebarWithHeader>
       <Switch>
@@ -16,8 +16,14 @@ export function Vault() {
         </Route>
         <Route
           path="/list/:item"
-          children={({ location }) => <Item data={location.state.data} />}
+          children={({ location }) => (
+            //@ts-expect-error
+            <ItemSettings data={location.state.data} />
+          )}
         />
+        <Route path="/settings">
+          <VaultSettings />
+        </Route>
       </Switch>
     </SidebarWithHeader>
   )
