e6d37122b0a7c2d42bd0a5b30cc5109881ffa9b4
Petr Spac <petrik.spac@gmail.com>
2021-10-17 11:25:19 +0200

    Mobile app/package update&folder structure (#54)
    * folder structure
    
    * reanimated 2, react navigation v6
    
    * List layaout change

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -7,38 +7,38 @@
  *
  * @format
  */
-import React, { useEffect } from 'react';
-import 'react-native-gesture-handler';
-import { Providers } from './src/Providers';
-import messaging from '@react-native-firebase/messaging';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect } from 'react'
+import 'react-native-gesture-handler'
+import { Providers } from './src/Providers'
+import messaging from '@react-native-firebase/messaging'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const App = () => {
   async function requestUserPermission() {
     //const token = await messaging().getToken();
-    const authStatus = await messaging().requestPermission();
+    const authStatus = await messaging().requestPermission()
     const enabled =
       authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL
 
     if (enabled) {
-      console.log('Authorization status:', authStatus);
+      console.log('Authorization status:', authStatus)
     }
   }
 
   useEffect(() => {
-    requestUserPermission();
+    requestUserPermission()
     const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-      let data = JSON.stringify(remoteMessage);
+      let data = JSON.stringify(remoteMessage)
       await AsyncStorage.setItem('notifies', data, (e) => {
-        if (e) console.log(e);
-      });
-    });
+        if (e) console.log(e)
+      })
+    })
 
-    return unsubscribe;
-  }, []);
+    return unsubscribe
+  }, [])
 
-  return <Providers />;
-};
+  return <Providers />
+}
 
-export default App;
+export default App
--- mobile-app/src/Providers.tsx
+++ mobile-app/src/Providers.tsx
@@ -1,35 +1,30 @@
-import React, {
-  createContext,
-  Dispatch,
-  SetStateAction,
-  useState,
-} from 'react';
-import { ApolloProvider } from '@apollo/client';
-import { NavigationContainer } from '@react-navigation/native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React, { createContext, Dispatch, SetStateAction, useState } from 'react'
+import { ApolloProvider } from '@apollo/client'
+import { NavigationContainer } from '@react-navigation/native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
-import HomeScreen from './navigation/HomeScreen';
-import { apoloCLient } from './ApolloClient';
-import TokenProvider from './TokenProvider';
-import { NativeBaseProvider } from 'native-base';
-import { theme } from './Theme';
-import NotifyProvider from './NotifyProvider';
+import HomeScreen from './navigation/HomeScreen'
+import { apoloCLient } from './ApolloClient'
+import TokenProvider from './providers/TokenProvider'
+import { NativeBaseProvider } from 'native-base'
+import { theme } from './Theme'
+import NotifyProvider from './providers/NotifyProvider'
 
 export const AuthsContext = createContext<{
-  auths: Array<any>;
+  auths: Array<any>
   setAuths: Dispatch<
     SetStateAction<{ secret: string; label: string; icon: string }[]>
-  >;
-}>({ auths: [] } as any);
+  >
+}>({ auths: [] } as any)
 
 export const Providers: React.FC<{}> = () => {
   const [auths, setAuths] = useState([
     {
       secret: 'JBSWY3DPEHPK3PXP',
       label: 'bitfinex',
-      icon: 'https://chakra-ui.com/favicon.png',
-    },
-  ]);
+      icon: 'https://chakra-ui.com/favicon.png'
+    }
+  ])
 
   return (
     <SafeAreaProvider>
@@ -47,5 +42,5 @@ export const Providers: React.FC<{}> = () => {
         </ApolloProvider>
       </NativeBaseProvider>
     </SafeAreaProvider>
-  );
-};
+  )
+}
