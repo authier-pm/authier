ccde40df2b0ad5c3c891344b88b3206176345999
Rambo <petrik.spac@gmail.com>
2021-10-24 11:08:58 +0200

    more typos

--- mobile-app/__tests__/Login.spec.tsx
+++ mobile-app/__tests__/Login.spec.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import { wrapInProviders } from '../test/providers'
+import { makeSsrClient, wrapInProviders } from '../test/providers'
 import renderer from 'react-test-renderer'
 import { Login } from '../src/screens/Login'
 
 it('Home component renders', () => {
-  const tree = renderer.create(wrapInProviders(<Login />)).toJSON()
+  const ac = makeSsrClient({})
+  const tree = renderer
+    .create(wrapInProviders(<Login navigation={() => {}} />, ac))
+    .toJSON()
   expect(tree).toMatchSnapshot()
 })
