af8063acf63fa69acd81dae1c9178b36dfcfb918
Rambo <petrik.spac@gmail.com>
2021-10-24 11:13:56 +0200

    more typos v.2

--- mobile-app/__tests__/Home.spec.tsx
+++ mobile-app/__tests__/Home.spec.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Home from '../src/screens/Home'
-import { wrapInProviders } from '../test/providers'
+import { makeSsrClient, wrapInProviders } from '../test/providers'
 import renderer from 'react-test-renderer'
 
 it('Home component renders', () => {
-  const tree = renderer.create(wrapInProviders(<Home />)).toJSON()
+  const ac = makeSsrClient({})
+  const tree = renderer.create(wrapInProviders(<Home />, ac)).toJSON()
   expect(tree).toMatchSnapshot()
 })
