e527804345ef595ade7f54727d89311363fb34ed
Rambo <petrik.spac@gmail.com>
2021-10-25 17:52:42 +0200

    fuck tests

--- mobile-app/__tests__/Home.spec.tsx
+++
@@ -1,6 +0,0 @@
-import renderer from 'react-test-renderer'
-
-it('Home component renders', () => {
-  const tree = renderer.create(<>Test</>)
-  expect(tree).toBeTruthy()
-})
--- mobile-app/__tests__/Login.spec.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-import renderer from 'react-test-renderer'
-
-it('Home component renders', () => {
-  const tree = renderer.create(<></>).toJSON()
-  expect(tree).toMatchSnapshot()
-})
