7b4efd3599cac0c7243fd332f3a5f8e34f1481eb
Jiri Spac <capajj@gmail.com>
2021-10-29 11:33:20 +0200

    add necessary security and monitoring tools in place for fastify (#59)

---
+++ backend/healthReportRoute.ts
@@ -0,0 +1,18 @@
+import os from 'os'
+import ip from 'ip'
+import ms from 'ms'
+import bytes from 'bytes'
+import v8 from 'v8'
+
+export const healthReportHandler = async (_request, reply) => {
+  reply.send({
+    ip: ip.address(),
+    freemem: bytes(os.freemem()),
+    totalmem: bytes(os.totalmem()),
+    maxOldSpaceSize: v8.getHeapStatistics().total_available_size / 1024 / 1024,
+    nodeVersion: process.version,
+    processUptime: ms(process.uptime() * 1000),
+    osUptime: ms(os.uptime() * 1000),
+    osTime: new Date().toISOString()
+  })
+}
