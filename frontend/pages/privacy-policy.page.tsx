57b98b21e63b09bff85727fc1fd62943bd8d322f
Jiri Spac <capajj@gmail.com>
2021-11-02 13:13:10 +0100

    introduce AuPage component (#60)

--- frontend/pages/privacy-policy.page.tsx
+++ frontend/pages/privacy-policy.page.tsx
@@ -2,9 +2,10 @@ import { Box } from '@chakra-ui/react'
 import Head from 'next/head'
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
-import { PageHeading } from '../components/PageHeading'
+import { AuPage } from '../components/AuPage'
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer'
 import md from './privacy-policy.md'
+import { t } from '@lingui/macro'
 
 // generated from https://app.freeprivacypolicy.com/download/f9567bd2-6453-4f1c-9e73-cd22e72d8fac
 export default function Privacy() {
@@ -13,10 +14,11 @@ export default function Privacy() {
       <Head>
         <title>Authier - privacy</title>
       </Head>
-      <PageHeading>Privacy policy</PageHeading>
-      <Box m={3} p={3}>
-        <ReactMarkdown components={ChakraUIRenderer()} children={md} />
-      </Box>
+      <AuPage heading={t`Privacy policy`}>
+        <Box m={3} p={3}>
+          <ReactMarkdown components={ChakraUIRenderer()} children={md} />
+        </Box>
+      </AuPage>
     </>
   )
 }
