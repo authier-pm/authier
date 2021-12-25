04ad506c1c383549f08eaa88038e035bba5eedf9
sleaper <petrik.spac@gmail.com>
2021-12-25 23:14:28 +0100

    started rewriting

--- mobile-app/android/app/src/main/java/com/authier/util/Helper.java
+++ mobile-app/android/app/src/main/java/com/authier/util/Helper.java
@@ -1,11 +1,47 @@
 package com.authier.util;
 
 import android.app.ActivityManager;
+import android.app.assist.AssistStructure;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
 import android.content.Context;
+import android.os.Build;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import com.authier.MainApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 public class Helper {
+    private static final String TAG = "DebugService";
+
+    public static void sendEvent(String evetName, String data, MainApplication application) {
+        ReactNativeHost reactNativeHost = application.getReactNativeHost();
+        ReactInstanceManager reactInstanceManager = reactNativeHost.getReactInstanceManager();
+        ReactContext reactContext = reactInstanceManager.getCurrentReactContext();
+
+        if (reactContext != null) {
+            WritableMap params = Arguments.createMap();
+            params.putString(evetName, data);
+            reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                    .emit(evetName, params);
+        }
+    }
+
     public static boolean isAppOnForeground(Context context) {
         /**
          We need to check if app is in foreground otherwise the app will crash.
@@ -26,5 +62,115 @@ public class Helper {
             }
         }
         return false;
+    }
+
+    public static String retriveNewApp(Context context) {
+        if (Build.VERSION.SDK_INT >= 21) {
+            String currentApp = null;
+            UsageStatsManager usm = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+
+            long time = System.currentTimeMillis();
+            List<UsageStats> applist = usm.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, time - 1000 * 1000, time);
+
+            if (applist != null && applist.size() > 0) {
+                SortedMap<Long, UsageStats> mySortedMap = new TreeMap<>();
+                for (UsageStats usageStats : applist) {
+                    mySortedMap.put(usageStats.getLastTimeUsed(), usageStats);
+                }
+                if (mySortedMap != null && !mySortedMap.isEmpty()) {
+                    currentApp = mySortedMap.get(mySortedMap.lastKey()).getPackageName();
+                }
+            }
+            return currentApp;
+
+        } else {
+
+            ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            String mm=(manager.getRunningTasks(1).get(0)).topActivity.getPackageName();
+
+            return mm;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Nullable
+    public static String getHint(@NonNull AssistStructure.ViewNode node) {
+
+        // First try the explicit autofill hints...
+        String[] hints = node.getAutofillHints();
+        if (hints != null) {
+            // We're simple, we only care about the first hint
+            return hints[0].toLowerCase();
+        }
+
+        // Then try some rudimentary heuristics based on other node properties
+        String viewHint = node.getHint();
+        String hint = inferHint(node, viewHint);
+
+        if (hint != null) {
+            Log.d(TAG, "Found hint using view hint(" + viewHint + "): " + hint);
+            return hint;
+        } else if (!TextUtils.isEmpty(viewHint)) {
+            Log.v(TAG, "No hint using view hint: " + viewHint);
+        }
+
+        String resourceId = node.getIdEntry();
+        hint = inferHint(node, resourceId);
+        if (hint != null) {
+            Log.d(TAG, "Found hint using resourceId(" + resourceId + "): " + hint);
+            return hint;
+        } else if (!TextUtils.isEmpty(resourceId)) {
+            Log.v(TAG, "No hint using resourceId: " + resourceId);
+        }
+
+        CharSequence text = node.getText();
+        CharSequence className = node.getClassName();
+        if (text != null && className != null && className.toString().contains("EditText")) {
+            hint = inferHint(node, text.toString());
+            if (hint != null) {
+                // NODE: text should not be logged, as it could contain PII
+                Log.d(TAG, "Found hint using text(" + text + "): " + hint);
+                return hint;
+            }
+        } else if (!TextUtils.isEmpty(text)) {
+            // NODE: text should not be logged, as it could contain PII
+            Log.v(TAG, "No hint using text: " + text + " and class " + className);
+        }
+        return null;
+    }
+
+    /**
+     * Uses heuristics to infer an autofill hint from a {@code string}.
+     * (Searching for some default hints like password or username)
+     * @return standard autofill hint, or {@code null} when it could not be inferred.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Nullable
+    protected static String inferHint(AssistStructure.ViewNode node, @Nullable String actualHint) {
+        if (actualHint == null) return null;
+
+        String hint = actualHint.toLowerCase();
+        if (hint.contains("label") || hint.contains("container")) {
+            Log.v(TAG, "Ignoring 'label/container' hint: " + hint);
+            return null;
+        }
+
+        if (hint.contains("password")) return View.AUTOFILL_HINT_PASSWORD;
+        if (hint.contains("username")
+                || (hint.contains("login") && hint.contains("id")))
+            return View.AUTOFILL_HINT_USERNAME;
+        if (hint.contains("email")) return View.AUTOFILL_HINT_EMAIL_ADDRESS;
+        if (hint.contains("name")) return View.AUTOFILL_HINT_NAME;
+        if (hint.contains("phone")) return View.AUTOFILL_HINT_PHONE;
+
+        // When everything else fails, return the full string - this is helpful to help app
+        // developers visualize when autofill is triggered when it shouldn't (for example, in a
+        // chat conversation window), so they can mark the root view of such activities with
+        // android:importantForAutofill=noExcludeDescendants
+        if (node.isEnabled() && node.getAutofillType() != View.AUTOFILL_TYPE_NONE) {
+            Log.v(TAG, "Falling back to " + actualHint);
+            return actualHint;
+        }
+        return null;
     }
 }
