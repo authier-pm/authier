3e9afe1bad8b0a179a21b751ee8e7fdb8eda877a
sleaper <petrik.spac@gmail.com>
2021-12-26 16:06:35 +0100

    added all native files

--- mobile-app/android/app/src/main/java/com/authier/MyAutofillService.java
+++ mobile-app/android/app/src/main/java/com/authier/MyAutofillService.java
@@ -14,15 +14,18 @@ import android.os.Bundle;
 import android.os.CancellationSignal;
 import android.provider.Settings;
 import android.service.autofill.AutofillService;
+import android.service.autofill.Dataset;
 import android.service.autofill.FillCallback;
 import android.service.autofill.FillContext;
 import android.service.autofill.FillRequest;
 import android.service.autofill.FillResponse;
 import android.service.autofill.SaveCallback;
+import android.service.autofill.SaveInfo;
 import android.service.autofill.SaveRequest;
 import android.util.ArrayMap;
 import android.util.Log;
 import android.view.autofill.AutofillId;
+import android.view.autofill.AutofillValue;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
@@ -34,7 +37,9 @@ import com.facebook.react.bridge.NativeModule;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -48,6 +53,7 @@ public class MyAutofillService extends AutofillService implements NativeModule {
     private static MainApplication sApplication;
     private boolean mAuthenticateResponses;
     private boolean mAuthenticateDatasets;
+    private int nodeCount = 1;
 
 
     @Override
@@ -94,6 +100,7 @@ public class MyAutofillService extends AutofillService implements NativeModule {
     }
 
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     public void onFillRequest(@NonNull FillRequest request, @NonNull CancellationSignal cancellationSignal, @NonNull FillCallback fillCallback) {
         //For now we are using sharedPreferences it is not ideal, we should at least encrypt it
@@ -140,7 +147,69 @@ public class MyAutofillService extends AutofillService implements NativeModule {
 
     @Override
     public void onSaveRequest(@NonNull SaveRequest saveRequest, @NonNull SaveCallback saveCallback) {
+        // Get the structure from the request
+        List<FillContext> context = saveRequest.getFillContexts();
 
+        AssistStructure structure = context.get(context.size() - 1).getStructure();
+
+        Object fields = null;
+        try {
+            fields = getSavaFields(structure);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        data.put(fields);
+        Log.e("OnSaveRequest", data.toString());
+
+        SharedPreferences.Editor e = myPrefs.edit();
+        e.putString("data", data.toString());
+        e.apply();
+
+        Helper.sendEvent("pswSaved", data.toString(), sApplication);
+
+        // Persist the data, if there are no errors, call onSuccess()
+        saveCallback.onSuccess();
+    }
+
+    @NonNull
+    private Object getSavaFields(@NonNull AssistStructure structure) throws JSONException {
+        JSONObject jsonObj= new JSONObject();
+
+        int nodes = structure.getWindowNodeCount();
+        for (int i = 0; i < nodes; i++) {
+            AssistStructure.ViewNode node = structure.getWindowNodeAt(i).getRootViewNode();
+            addFieldsToSave(node, jsonObj);
+        }
+
+        return jsonObj;
+    }
+
+    private void addFieldsToSave(@NonNull AssistStructure.ViewNode node, JSONObject obj) throws JSONException {
+        String androidUriToSave;
+        String hint = Helper.getHint(node);
+
+        if (hint != null) {
+
+            androidUriToSave = Helper.retriveNewApp(sApplication);
+
+            obj.put("androidUri", androidUriToSave);
+            if(nodeCount == 1) {
+                obj.put("usernameHint", hint);
+                obj.put("username", node.getText());
+            } else if(nodeCount == 2){
+                obj.put("passwordHint", hint);
+                obj.put("password", node.getText());
+            }
+
+            nodeCount = nodeCount + 1;
+        }
+
+
+        int childrenSize = node.getChildCount();
+        for (int i = 0; i < childrenSize; i++) {
+            addFieldsToSave( node.getChildAt(i), obj);
+        }
     }
 
     /**
@@ -190,6 +259,88 @@ public class MyAutofillService extends AutofillService implements NativeModule {
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    static FillResponse createResponse(@NonNull Context context,
+                                       @NonNull ArrayMap<String, AutofillId> fields) throws JSONException {
+
+        String packageName = context.getPackageName();
+        FillResponse.Builder response = new FillResponse.Builder();
+
+        if(data != null) {
+            for (int i = 0; i < data.length(); i++) {
+                JSONObject item = data.getJSONObject(i);
+                if(item.getString("androidUri").equals(androidUri)) {
+                    Dataset unlockedDataset = fieldDataset(fields, packageName, item);
+                    response.addDataset(unlockedDataset);
+                }
+            }
+        }
+
+        Collection<AutofillId> ids = fields.values();
+        AutofillId[] requiredIds = new AutofillId[ids.size()];
+        ids.toArray(requiredIds);
+        response.setSaveInfo(
+                // We're simple, so we're generic
+                new SaveInfo.Builder(SaveInfo.SAVE_DATA_TYPE_USERNAME | SaveInfo.SAVE_DATA_TYPE_PASSWORD, requiredIds).setFlags(SaveInfo.FLAG_SAVE_ON_ALL_VIEWS_INVISIBLE).build());
+
+
+        // 3.Profit!
+        return response.build();
+    }
+
+    static Dataset fieldDataset(@NonNull Map<String, AutofillId> fields,
+                                @NonNull String packageName, JSONObject item) throws JSONException {
+        Dataset.Builder dataset = new Dataset.Builder();
+
+        for (Map.Entry<String, AutofillId> field : fields.entrySet()) {
+            String hint = field.getKey();
+            AutofillId id = field.getValue();
+
+            RemoteViews presentation = newDatasetPresentation(packageName, item.getString("username"));
+
+            if(hint.equals(item.getString("usernameHint"))) {
+
+                dataset.setValue(id, AutofillValue.forText(item.getString("username")), presentation);
+            } else if(hint.equals(item.getString("passwordHint"))) {
+
+                dataset.setValue(id, AutofillValue.forText(item.getString("password")), presentation);
+            }
+
+        }
+
+        return dataset.build();
+    }
+
+    /**
+     * Helper method to create a dataset presentation with the given text.
+     */
+    @NonNull
+    static RemoteViews newDatasetPresentation(@NonNull String packageName,
+                                              @NonNull CharSequence text) {
+        RemoteViews presentation =
+                new RemoteViews(packageName, R.layout.multidataset_service_list_item);
+        presentation.setTextViewText(R.id.text, text);
+        presentation.setImageViewResource(R.id.icon, R.mipmap.ic_launcher);
+        return presentation;
+    }
+
+    /**
+     * Displays a toast with the given message.
+     */
+    private void toast(@NonNull CharSequence message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * Helper method to get the {@link AssistStructure} associated with the latest request
+     * in an autofill context.
+     */
+    @NonNull
+    static AssistStructure getLatestAssistStructure(@NonNull FillRequest request) {
+        List<FillContext> fillContexts = request.getFillContexts();
+        return fillContexts.get(fillContexts.size() - 1).getStructure();
+    }
+
     @NonNull
     @Override
     public String getName() {
@@ -211,22 +362,7 @@ public class MyAutofillService extends AutofillService implements NativeModule {
 
     }
 
-    /**
-     * Displays a toast with the given message.
-     */
-    private void toast(@NonNull CharSequence message) {
-        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
-    }
 
-    /**
-     * Helper method to get the {@link AssistStructure} associated with the latest request
-     * in an autofill context.
-     */
-    @NonNull
-    static AssistStructure getLatestAssistStructure(@NonNull FillRequest request) {
-        List<FillContext> fillContexts = request.getFillContexts();
-        return fillContexts.get(fillContexts.size() - 1).getStructure();
-    }
 
 
 }
