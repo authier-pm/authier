f53d747c6ac127f4d27a43f7f2a86a4a12aa5456
Rambo <petrik.spac@gmail.com>
2022-01-01 19:01:36 +0100

    more fixes

--- web-extension/src/vault-index.tsx
+++ web-extension/src/vault-index.tsx
@@ -6,7 +6,7 @@ import browser from 'webextension-polyfill'
 import { ApolloProvider } from '@apollo/client'
 import App from './App'
 import { apolloClient } from './apollo/apolloClient'
-import { HashRouter } from 'react-router-dom'
+import { HashRouter, BrowserRouter } from 'react-router-dom'
 
 Sentry.init({
   dsn: 'https://528d6bfc04eb436faea6046afc419f56@o997539.ingest.sentry.io/5955889'
