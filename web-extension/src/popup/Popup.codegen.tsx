412ee06d193a68591117acd9bead80415bd87897
Jiri Spac <capajj@gmail.com>
2022-01-01 16:53:37 +0100

    fix web extension login/logout flow (#71)
    * fix web extension login/logout flow
    
    * fix backend TSC
    
    * attempt to fix the spec
    
    * second fix for RootResolver spec
    
    * fix specs
    
    * add test script with watch
    
    * remove useless react-router-dom
    
    * add lastGeoLocation resolver
    
    * fix TSC for web extension

--- web-extension/src/popup/Popup.codegen.tsx
+++ web-extension/src/popup/Popup.codegen.tsx
@@ -3,11 +3,6 @@ import * as Types from '../../../shared/generated/graphqlBaseTypes';
 import { gql } from '@apollo/client';
 import * as Apollo from '@apollo/client';
 const defaultOptions =  {}
-export type IsLoggedInQueryVariables = Types.Exact<{ [key: string]: never; }>;
-
-
-export type IsLoggedInQuery = { __typename?: 'Query', authenticated: boolean };
-
 export type SettingsQueryVariables = Types.Exact<{ [key: string]: never; }>;
 
 
@@ -15,7 +10,7 @@ export type SettingsQuery = { __typename?: 'Query', me?: { __typename?: 'UserQue
 
 export type SendAuthMessageQueryVariables = Types.Exact<{
   device: Types.Scalars['String'];
-  time: Types.Scalars['String'];
+  time: Types.Scalars['DateTime'];
   location: Types.Scalars['String'];
   pageName: Types.Scalars['String'];
 }>;
@@ -31,38 +26,6 @@ export type SaveFirebaseTokenMutationVariables = Types.Exact<{
 export type SaveFirebaseTokenMutation = { __typename?: 'Mutation', me?: { __typename?: 'UserMutation', updateFireToken: { __typename?: 'DeviceGQL', id: string } } | null | undefined };
 
 
-export const IsLoggedInDocument = gql`
-    query isLoggedIn {
-  authenticated
-}
-    `;
-
-/**
- * __useIsLoggedInQuery__
- *
- * To run a query within a React component, call `useIsLoggedInQuery` and pass it any options that fit your needs.
- * When your component renders, `useIsLoggedInQuery` returns an object from Apollo Client that contains loading, error, and data properties
- * you can use to render your UI.
- *
- * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
- *
- * @example
- * const { data, loading, error } = useIsLoggedInQuery({
- *   variables: {
- *   },
- * });
- */
-export function useIsLoggedInQuery(baseOptions?: Apollo.QueryHookOptions<IsLoggedInQuery, IsLoggedInQueryVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<IsLoggedInQuery, IsLoggedInQueryVariables>(IsLoggedInDocument, options);
-      }
-export function useIsLoggedInLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<IsLoggedInQuery, IsLoggedInQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<IsLoggedInQuery, IsLoggedInQueryVariables>(IsLoggedInDocument, options);
-        }
-export type IsLoggedInQueryHookResult = ReturnType<typeof useIsLoggedInQuery>;
-export type IsLoggedInLazyQueryHookResult = ReturnType<typeof useIsLoggedInLazyQuery>;
-export type IsLoggedInQueryResult = Apollo.QueryResult<IsLoggedInQuery, IsLoggedInQueryVariables>;
 export const SettingsDocument = gql`
     query settings {
   me {
@@ -103,7 +66,7 @@ export type SettingsQueryHookResult = ReturnType<typeof useSettingsQuery>;
 export type SettingsLazyQueryHookResult = ReturnType<typeof useSettingsLazyQuery>;
 export type SettingsQueryResult = Apollo.QueryResult<SettingsQuery, SettingsQueryVariables>;
 export const SendAuthMessageDocument = gql`
-    query sendAuthMessage($device: String!, $time: String!, $location: String!, $pageName: String!) {
+    query sendAuthMessage($device: String!, $time: DateTime!, $location: String!, $pageName: String!) {
   me {
     sendAuthMessage(
       device: $device
