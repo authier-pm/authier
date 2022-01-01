4942f3e8fc565a470c8597a0291ebd2d128abe51
Rambo <petrik.spac@gmail.com>
2022-01-01 19:01:36 +0100

    more fixes
    Former-commit-id: f53d747c6ac127f4d27a43f7f2a86a4a12aa5456

--- web-extension/src/pages-vault/Vault.tsx
+++ web-extension/src/pages-vault/Vault.tsx
@@ -1,25 +1,23 @@
 import React from 'react'
 import SidebarWithHeader from '../components/vault/SidebarWithHeader'
 import { ItemList } from '@src/components/vault/ItemList'
-import { Route, useLocation, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import { ItemSettings } from '@src/components/vault/ItemSettings'
 import { VaultSettings } from './VaultSettings'
 
 export function Vault() {
-  const location = useLocation()
-
   return (
     <SidebarWithHeader>
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <ItemList />
         </Route>
         <Route
           path="/list/:item"
-          children={({ location }) => {
-            console.log(location)
-            return <ItemSettings {...location.state.data} />
-          }}
+          children={({ location }) => (
+            //@ts-expect-error
+            <ItemSettings {...location.state.data} />
+          )}
         />
         <Route path="/settings">
           <VaultSettings />
