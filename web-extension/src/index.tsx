3add5945b1870b9c06198513b2763e43c25d6b67
Jiří Špác <capajj@gmail.com>
2022-01-06 13:16:24 +0100

    more cleanup and fixes
    Former-commit-id: 6221a88dac1a6c24111d4e289b4fdce827d45006 [formerly 63617aa8f082f781ac24c18e3f518a75a09c85e4]
    Former-commit-id: 45103f00fba388a9876ca14270aa28be8e47be72

--- web-extension/src/index.tsx
+++ web-extension/src/index.tsx
@@ -7,31 +7,20 @@ import { ApolloProvider } from '@apollo/client'
 import App from './App'
 import { apolloClient } from './apollo/apolloClient'
 import { ColorModeScript, theme } from '@chakra-ui/react'
-import { BackgroundMessageType } from './background/BackgroundMessageType'
 
 Sentry.init({
   dsn: 'https://528d6bfc04eb436faea6046afc419f56@o997539.ingest.sentry.io/5955889'
 })
 
 export const renderPopup = () => {
-  const popupElement = document.getElementById('popup')
+  const popupRoot = document.getElementById('popup')
 
-  if (!popupElement) {
-    return
-  }
   ReactDOM.render(
     <ApolloProvider client={apolloClient}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App parent="popup" />
     </ApolloProvider>,
-    popupElement
+    popupRoot
   )
 }
 browser.tabs.query({ active: true, currentWindow: true }).then(renderPopup)
-
-browser.runtime.connect({ name: 'popup' })
-browser.runtime.onMessage.addListener((msg) => {
-  if (msg.action === BackgroundMessageType.rerenderViews) {
-    renderPopup()
-  }
-})
