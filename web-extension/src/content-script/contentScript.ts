131e9b3cd58830ed13e56207a557751266cde252
Jiří Špác <capajj@gmail.com>
2022-01-07 19:29:08 +0100

    fix one bug with unescaped HTML in renderSaveCredentialsForm
    Former-commit-id: 0a6ec6fccbd65cf267593a9336c9953df5ec8300 [formerly bfb08c54ea73cd1a0457eb7e3ad380a14c8d1eba]
    Former-commit-id: 3e51fe7f114af1297221350f2e720e3195cae0a5

--- web-extension/src/content-script/contentScript.ts
+++ web-extension/src/content-script/contentScript.ts
@@ -2,7 +2,6 @@ import { BackgroundMessageType } from '@src/background/BackgroundMessageType'
 
 import { debounce } from 'lodash'
 import browser from 'webextension-polyfill'
-import { authierColors } from '../../../shared/chakraCustomTheme'
 
 import { DOMEventsRecorder, IInputRecord } from './DOMEventsRecorder'
 import debug from 'debug'
@@ -11,6 +10,10 @@ import { WebInputType } from '../../../shared/generated/graphqlBaseTypes'
 import { ILoginSecret, ITOTPSecret } from '@src/util/useDeviceState'
 import { bodyInputChangeEmitter } from './DOMObserver'
 import { autofill, IDecryptedSecrets } from './autofill'
+import {
+  promptDiv,
+  renderSaveCredentialsForm
+} from './renderSaveCredentialsForm'
 
 const log = debug('au:contentScript')
 localStorage.debug = localStorage.debug || 'au:*' // enable all debug messages
@@ -22,7 +25,7 @@ const inputKindMap = {
 
 export interface IInitStateRes {
   extensionDeviceReady: boolean
-  secretsForHost?: IDecryptedSecrets
+  secretsForHost: IDecryptedSecrets
   saveLoginModalsState?:
     | {
         password: string
@@ -43,23 +46,24 @@ export function getWebInputKind(
   )
 }
 
-const domRecorder = new DOMEventsRecorder()
+export const domRecorder = new DOMEventsRecorder()
 
 const formsRegisteredForSubmitEvent = [] as HTMLFormElement[]
 export async function initInputWatch() {
   const stateInitRes: IInitStateRes = await browser.runtime.sendMessage({
     action: BackgroundMessageType.getContentScriptInitialState
   })
+  if (!stateInitRes) {
+    return
+  }
 
   const { saveLoginModalsState, extensionDeviceReady, secretsForHost } =
     stateInitRes
 
   if (!extensionDeviceReady) {
-    return // no need to do anything
+    return // no need to do anything-user locked out
   }
-  if (secretsForHost) {
-    autofill(secretsForHost)
-  }
+  autofill(secretsForHost)
 
   if (
     saveLoginModalsState &&
@@ -185,119 +189,5 @@ export async function initInputWatch() {
   document.body.addEventListener('input', debouncedInputEventListener, true) // maybe there are websites where this won't work, we need to test this out larger number of websites
 }
 
-let promptDiv: HTMLDivElement | null
-
-function renderSaveCredentialsForm(username: string, password: string) {
-  promptDiv = document.createElement('div')
-
-  promptDiv.id = '__AUTHIER__savePrompt'
-  promptDiv.style.zIndex = '2147483647' // max z-index according to stackoverflow
-  promptDiv.style.display = 'flex'
-  promptDiv.style.justifyContent = 'center'
-  promptDiv.style.alignItems = 'baseline'
-  promptDiv.style.fontFamily = 'sans-serif !important'
-
-  promptDiv.style.width = '100%'
-  promptDiv.style.position = 'fixed'
-  promptDiv.style.padding = '8px'
-  promptDiv.style.backgroundColor = authierColors.green[300]
-  promptDiv.style.top = '0px'
-  promptDiv.style.left = '0px'
-  const buttonStyle = (bgColor: string) =>
-    `background-color:${bgColor}; color: ${authierColors.green[100]}; margin: 4px; padding: 4px;border-radius: 4px;`
-
-  const h3Style = '"margin: 0 5px; font-family: sans-serif !important;"'
-  promptDiv.innerHTML = `
-  <span>Username: </span><h3 style=${h3Style}>${username}</h3>
-  <span>Password: </span><h3 id="__AUTHIER__pswdDisplay" style=${h3Style}>${password.replaceAll(
-    /./g,
-    '*'
-  )}</h3>
-<button id="__AUTHIER__showPswdBtn" style="${buttonStyle(
-    authierColors.teal[100]
-  )}" >👁️</button >
- <div style="margin: 0 15px;">
-
- <button id="__AUTHIER__saveBtn" style="${buttonStyle(
-   authierColors.green[500]
- )} min-width=60px;">save</button >
-  <button id="__AUTHIER__saveAndEditBtn" style="${buttonStyle(
-    authierColors.green[600]
-  )} min-width=60px;">save & edit</button >
-  <button style="${buttonStyle(
-    authierColors.teal[900]
-  )}" id="__AUTHIER__closeBtn">close</button >
- </div>
-  `
-
-  document.body.appendChild(promptDiv)
-
-  browser.runtime.sendMessage({
-    action: BackgroundMessageType.saveLoginCredentialsModalShown,
-    payload: {
-      username,
-      password,
-      capturedInputEvents: domRecorder.toJSON()
-    }
-  })
-
-  function closePrompt() {
-    promptDiv!.remove()
-    promptDiv = null
-  }
-
-  const addCredential = async (openInVault = false) => {
-    const loginCredentials = {
-      username,
-      password,
-      capturedInputEvents: domRecorder.toJSON(),
-      openInVault
-    }
-    return browser.runtime.sendMessage({
-      action: BackgroundMessageType.addLoginCredentials,
-      payload: loginCredentials
-    })
-  }
-  document
-    .querySelector('#__AUTHIER__saveBtn')
-    ?.addEventListener('click', async () => {
-      await addCredential()
-      closePrompt()
-    })
-
-  document
-    .querySelector('#__AUTHIER__saveAndEditBtn')
-    ?.addEventListener('click', async () => {
-      await addCredential(true)
-
-      closePrompt()
-    })
-  document
-    .querySelector('#__AUTHIER__closeBtn')
-    ?.addEventListener('click', async () => {
-      closePrompt()
-
-      browser.runtime.sendMessage({
-        action: BackgroundMessageType.hideLoginCredentialsModal
-      })
-    })
-
-  // password show button functionality
-  let passwordShown = false
-  document
-    .querySelector('#__AUTHIER__showPswdBtn')
-    ?.addEventListener('click', () => {
-      const passwordDisplayEl = document.querySelector(
-        '#__AUTHIER__pswdDisplay'
-      )!
-      if (passwordShown) {
-        passwordDisplayEl.innerHTML = password.replaceAll(/./g, '*')
-        passwordShown = false
-      } else {
-        passwordDisplayEl.innerHTML = password
-        passwordShown = true
-      }
-    })
-}
 // showSavePrompt()
 initInputWatch()
