4f2b316b62cdf8488561233882587339cc02ad3e
Rambo <petrik.spac@gmail.com>
2022-01-08 20:03:06 +0100

    more UI

--- web-extension/src/components/vault/ItemSettings.tsx
+++ web-extension/src/components/vault/ItemSettings.tsx
@@ -29,6 +29,7 @@ import { device } from '@src/background/ExtensionDevice'
 import { useUpdateEncryptedSecretMutation } from './ItemSettings.codegen'
 import { Field, Form, Formik, FormikHelpers } from 'formik'
 import { Trans } from '@lingui/macro'
+import { motion } from 'framer-motion'
 
 enum Value {
   'Tooweak' = 1,
@@ -63,57 +64,64 @@ const TOTPSecret = (data: ITOTPSecret) => {
   }
   const handleClick = () => setShow(!show)
   return (
-    <Center
-      mt={4}
-      flexDirection="column"
-      boxShadow={'2xl'}
-      rounded={'md'}
-      overflow={'hidden'}
-      w={['400px', '600px']}
-      minW={'420px'}
-      m="auto"
-      bg={useColorModeValue('white', 'gray.900')}
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.25 }}
     >
-      <Flex p={5} flexDirection="column" w="inherit">
-        <SimpleGrid row={2} columns={2} spacing="40px">
-          <InputWithHeading heading="URL:" defaultValue={data.url} />
-          <InputWithHeading heading="Label:" defaultValue={data.label} />
+      <Center
+        mt={4}
+        flexDirection="column"
+        boxShadow={'2xl'}
+        rounded={'md'}
+        overflow={'hidden'}
+        w={['400px', '600px']}
+        minW={'420px'}
+        m="auto"
+        bg={useColorModeValue('white', 'gray.900')}
+      >
+        <Flex p={5} flexDirection="column" w="inherit">
+          <SimpleGrid row={2} columns={2} spacing="40px">
+            <InputWithHeading heading="URL:" defaultValue={data.url} />
+            <InputWithHeading heading="Label:" defaultValue={data.label} />
 
-          <Box flex={'50%'}>
-            <Heading size="md" as="h5">
-              Secret:
-            </Heading>
+            <Box flex={'50%'}>
+              <Heading size="md" as="h5">
+                Secret:
+              </Heading>
 
-            <InputGroup size="md">
-              <Input
-                value={secret}
-                onChange={handleChangeSecret}
-                pr="4.5rem"
-                type={show ? 'text' : 'password'}
-              />
-              <InputRightElement width="4.5rem">
-                <Button h="1.75rem" size="sm" onClick={handleClick}>
-                  {show ? 'Hide' : 'Show'}
-                </Button>
-              </InputRightElement>
-            </InputGroup>
-          </Box>
-        </SimpleGrid>
+              <InputGroup size="md">
+                <Input
+                  value={secret}
+                  onChange={handleChangeSecret}
+                  pr="4.5rem"
+                  type={show ? 'text' : 'password'}
+                />
+                <InputRightElement width="4.5rem">
+                  <Button h="1.75rem" size="sm" onClick={handleClick}>
+                    {show ? 'Hide' : 'Show'}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </Box>
+          </SimpleGrid>
 
-        <Stack direction={'row'} justifyContent="flex-end" spacing={1} my={5}>
-          <Button
-            colorScheme="blackAlpha"
-            size="sm"
-            onClick={() => history.goBack()}
-          >
-            Go back
-          </Button>
-          <Button colorScheme="twitter" size="sm">
-            Save
-          </Button>
-        </Stack>
-      </Flex>
-    </Center>
+          <Stack direction={'row'} justifyContent="flex-end" spacing={1} my={5}>
+            <Button
+              colorScheme="blackAlpha"
+              size="sm"
+              onClick={() => history.goBack()}
+            >
+              Go back
+            </Button>
+            <Button colorScheme="twitter" size="sm">
+              Save
+            </Button>
+          </Stack>
+        </Flex>
+      </Center>
+    </motion.div>
   )
 }
 
@@ -147,46 +155,51 @@ const LoginSecret = (data: ILoginSecret) => {
   const [updateSecret] = useUpdateEncryptedSecretMutation()
 
   return (
-    <Center
-      mt={4}
-      flexDirection="column"
-      boxShadow={'2xl'}
-      rounded={'md'}
-      overflow={'hidden'}
-      w={['400px', '600px']}
-      minW={'420px'}
-      m="auto"
-      bg={useColorModeValue('white', 'gray.900')}
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.35 }}
     >
-      <Formik
-        initialValues={{
-          url: data.url,
-          password: data.loginCredentials.password,
-          label: data.label,
-          username: data.loginCredentials.username
-        }}
-        onSubmit={async (
-          values: LoginParsedValues,
-          { setSubmitting }: FormikHelpers<LoginParsedValues>
-        ) => {
-          await updateSecret({
-            variables: {
-              id: data.id,
-              patch: {
-                //Finish here the string
-                encrypted: data.encrypted,
-                label: values.label,
-                url: values.url,
-                kind: data.kind
-              }
-            }
-          })
-          await device.state?.save()
-          setSubmitting(false)
-        }}
+      <Flex
+        mt={4}
+        flexDirection="column"
+        boxShadow={'2xl'}
+        rounded={'md'}
+        overflow={'hidden'}
+        w={['400px', '600px']}
+        minW={'420px'}
+        m="auto"
+        bg={useColorModeValue('white', 'gray.900')}
       >
-        {({ values, setSubmitting }) => (
-          <>
+        <Formik
+          initialValues={{
+            url: data.url,
+            password: data.loginCredentials.password,
+            label: data.label,
+            username: data.loginCredentials.username
+          }}
+          onSubmit={async (
+            values: LoginParsedValues,
+            { setSubmitting }: FormikHelpers<LoginParsedValues>
+          ) => {
+            await updateSecret({
+              variables: {
+                id: data.id,
+                patch: {
+                  //Finish here the string
+                  encrypted: data.encrypted,
+                  label: values.label,
+                  url: values.url,
+                  kind: data.kind
+                }
+              }
+            })
+            await device.state?.save()
+            setSubmitting(false)
+          }}
+        >
+          {({ values, setSubmitting }) => (
             <Flex p={5} as={Form} flexDirection="column" w="inherit">
               <SimpleGrid row={2} columns={2} spacing="40px">
                 <Field name="url">
@@ -294,22 +307,20 @@ const LoginSecret = (data: ILoginSecret) => {
                   Save
                 </Button>
               </Stack>
-
-              <>
-                <IconButton
-                  w="min-content"
-                  aria-label="Search database"
-                  icon={isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
-                  onClick={onToggle}
-                  m={3}
-                />
-                <PasswordGenerator isOpen={isOpen} />
-              </>
             </Flex>
-          </>
-        )}
-      </Formik>
-    </Center>
+          )}
+        </Formik>
+
+        <IconButton
+          w="min-content"
+          aria-label="Search database"
+          icon={isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+          onClick={onToggle}
+          m={3}
+        />
+        <PasswordGenerator isOpen={isOpen} />
+      </Flex>
+    </motion.div>
   )
 }
 
--- web-extension/src/pages-vault/VaultSettings.tsx
+++ web-extension/src/pages-vault/VaultSettings.tsx
@@ -1,83 +1,82 @@
-import { Flex, HStack, Link, useColorModeValue } from '@chakra-ui/react'
+import { Box, Flex, HStack, Link, useColorModeValue } from '@chakra-ui/react'
 import {
   Link as RouterLink,
   Route,
   Switch,
-  useRouteMatch
+  useRouteMatch,
+  useLocation
 } from 'react-router-dom'
-import React from 'react'
+import React, { useState } from 'react'
 import Account from '@src/components/vault/settings/Account'
 import Config from '@src/components/vault/settings/Config'
+import { AnimatePresence, motion } from 'framer-motion'
 
 interface LinkItemProps {
   name: string
   path: string
-  selected: boolean
 }
 
 interface Props extends LinkItemProps {
   url: string
   handleClick: () => void
+  selected: LinkItemProps
 }
 const LinkItems: Array<LinkItemProps> = [
-  { name: 'Account', path: '/account', selected: false },
-  { name: 'Config', path: '/config', selected: false }
+  { name: 'Account', path: '/account' },
+  { name: 'Config', path: '/config' }
 ]
 
 const NavLink = ({ name, path, handleClick, url, selected }: Props) => {
   return (
-    <Flex
+    <Box
       align="center"
       px={2}
       py={1}
       borderRadius="lg"
       role="group"
       cursor="pointer"
-      _hover={{
-        bg: useColorModeValue('gray.100', 'gray.700')
-      }}
       fontSize={20}
       bgColor={
-        selected ? useColorModeValue('gray.100', 'gray.700') : 'transparent'
+        selected.name === name
+          ? useColorModeValue('gray.100', 'gray.700')
+          : 'transparent'
       }
       onClick={() => handleClick()}
     >
       <Link as={RouterLink} to={url + path} style={{ textDecoration: 'none' }}>
         {name}
       </Link>
-    </Flex>
+    </Box>
   )
 }
 
 export const VaultSettings = () => {
   const { url } = useRouteMatch()
+  const location = useLocation()
+  const [selectedTab, setSelectedTab] = useState(LinkItems[0])
 
   return (
     <Flex
-      flexDirection={'column'}
-      m="auto"
-      maxW={'320px'}
-      w={'full'}
-      bg={useColorModeValue('white', 'gray.900')}
-      boxShadow={'2xl'}
       rounded={'lg'}
-      p={6}
-      textAlign={'center'}
+      boxShadow={'lg'}
+      p={5}
+      align={'center'}
+      justify={'center'}
+      flexDirection={'column'}
+      maxW={'4xl'}
+      m="auto"
+      bg={useColorModeValue('white', 'gray.900')}
     >
-      <HStack as={'nav'} spacing={4} display={{ base: 'none', md: 'flex' }}>
-        {LinkItems.map((link, i, arr) => {
+      <HStack as={'nav'} spacing={4}>
+        {LinkItems.map((link) => {
           const handleClick = () => {
-            arr.forEach((j) => {
-              j.selected ? (j.selected = false) : null
-            })
-            link.selected = true
+            setSelectedTab(link)
           }
-
           return (
             <NavLink
               key={link.name}
               path={link.path}
-              selected={link.selected}
+              selected={selectedTab}
               handleClick={handleClick}
               name={link.name}
               url={url}
@@ -86,14 +85,16 @@ export const VaultSettings = () => {
         })}
       </HStack>
 
-      <Switch>
-        <Route exact path={'/settings/account'}>
-          <Account />
-        </Route>
-        <Route path={'/settings/config'}>
-          <Config />
-        </Route>
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route exact path={'/settings/account'}>
+            <Account />
+          </Route>
+          <Route path={'/settings/config'}>
+            <Config />
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </Flex>
   )
 }
