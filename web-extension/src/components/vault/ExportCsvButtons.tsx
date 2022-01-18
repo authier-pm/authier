71bd9688eaa3f1cebcb38ef1db5e8ff94991962d
Jiri Spac <capajj@gmail.com>
2022-01-18 06:51:25 +0100

    vault export functionality (#111)

---
+++ web-extension/src/components/vault/ExportCsvButtons.tsx
@@ -0,0 +1,51 @@
+import { Button } from '@chakra-ui/react'
+import React, { useContext } from 'react'
+import papaparse from 'papaparse'
+
+import { downloadAsFile } from '@src/util/downloadAsFile'
+import { Trans } from '@lingui/macro'
+import { DeviceStateContext } from '@src/providers/DeviceStateProvider'
+
+export const ExportTOTPToCsvButton = () => {
+  const { TOTPSecrets } = useContext(DeviceStateContext)
+
+  return (
+    <Button
+      mt={4}
+      colorScheme="teal"
+      type="submit"
+      onClick={() => {
+        const csv = papaparse.unparse(TOTPSecrets)
+        downloadAsFile(csv, 'totp')
+      }}
+    >
+      <Trans>Export TOTP to CSV</Trans>
+    </Button>
+  )
+}
+
+export const ExportLoginCredentialsToCsvButton = () => {
+  const { LoginCredentials } = useContext(DeviceStateContext)
+
+  return (
+    <Button
+      mt={4}
+      colorScheme="teal"
+      type="submit"
+      onClick={() => {
+        const csv = papaparse.unparse(
+          LoginCredentials.map(({ url, label, id, loginCredentials }) => ({
+            id,
+            url,
+            label,
+            username: loginCredentials.username,
+            password: loginCredentials.password
+          }))
+        )
+        downloadAsFile(csv, 'totp')
+      }}
+    >
+      <Trans>Export Login Credentials to CSV</Trans>
+    </Button>
+  )
+}
