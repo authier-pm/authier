f21b2a9434277ac677717347cb4cefff3d8d1b42
Jiří Špác <capajj@gmail.com>
2022-01-18 15:57:34 +0100

    import from file-lastpass support for now
    Former-commit-id: 9511953a8abb0fc1a66ff83f1a71c32980a3bcfb [formerly 4e8b9aab5e67e6921bd451f27f684e0059d899bd]
    Former-commit-id: 8cb34dbbe0467ee4403e0ee58f3d60b066990197

--- web-extension/src/pages-vault/VaultImportExport.tsx
+++ web-extension/src/pages-vault/VaultImportExport.tsx
@@ -1,23 +1,72 @@
-import { Button, Flex, Heading } from '@chakra-ui/react'
+import { Box, Button, Flex, Heading } from '@chakra-ui/react'
 import { Trans } from '@lingui/macro'
 import {
   ExportLoginCredentialsToCsvButton,
   ExportTOTPToCsvButton
 } from '@src/components/vault/ExportCsvButtons'
+import { ImportFromFile } from '@src/components/vault/ImportFromFile'
 import React from 'react'
+import papaparse from 'papaparse'
+
+const mapCsvToLoginCredentials = (csv: string[][]) => {
+  const [header] = csv
+
+  const indexUsername = header.findIndex((x) => x === 'username')
+  const indexLabel = header.findIndex((x) => x === 'name')
+  const indexPassword = header.findIndex((x) => x === 'password')
+  const indexUrl = header.findIndex((x) => x === 'url')
+
+  if (
+    indexUsername === -1 ||
+    indexLabel === -1 ||
+    indexPassword === -1 ||
+    indexUrl === -1
+  ) {
+    return []
+  }
+
+  return csv.slice(1).map((row) => ({
+    url: row[indexUrl],
+    label: row[indexLabel],
+    loginCredential: {
+      username: row[indexUsername],
+      password: row[indexPassword]
+    }
+  }))
+}
 
 export const VaultImportExport: React.FC<{}> = () => {
+  const [isCompleted, setIsCompleted] = React.useState(false)
   return (
     <Flex p={5} flexDir={'column'}>
       <Heading size="sm">Import</Heading>
-      <Button
-        m={2}
-        onClick={() => {
-          alert('Not implemented')
-        }}
-      >
-        <Trans>Parse and add secrets</Trans>
-      </Button>
+      <Box p={30}>
+        {!isCompleted ? (
+          <ImportFromFile
+            onFileAccepted={(file) => {
+              papaparse.parse(file, {
+                complete: (results) => {
+                  const mapped = mapCsvToLoginCredentials(results.data)
+                  // TODO add to device state
+                  setIsCompleted(true)
+                }
+              })
+
+              // console.log('~ parsed', parsed, file)
+            }}
+          />
+        ) : (
+          <Button
+            m={2}
+            onClick={() => {
+              setIsCompleted(false)
+            }}
+          >
+            <Trans>Import another</Trans>
+          </Button>
+        )}
+      </Box>
+
       <Heading size="sm">Export</Heading>
 
       <ExportLoginCredentialsToCsvButton />
