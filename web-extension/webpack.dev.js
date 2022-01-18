e7eeb9e3e92f1bc18a009e40e8a369fc068d2ed3
Jiří Špác <capajj@gmail.com>
2022-01-18 01:52:27 +0100

    vault export functionality
    Former-commit-id: bc4014cb460dfacacc63806fe1eb586fa706222c [formerly 272b99791b3acc43f970468c72d073ecdd8e5997]
    Former-commit-id: 5cbdcb02b1461e2bd1dc6bae139f4215135e3b8a

--- web-extension/webpack.dev.js
+++ web-extension/webpack.dev.js
@@ -1,8 +1,15 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const webpack = require('webpack')
-
+const ExtensionReloader = require('webpack-extension-reloader')
+const path = require('path')
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map'
+
+  // plugins: [ // TODO fix
+  //   new ExtensionReloader({
+  //     manifest: path.resolve(__dirname, './dist/manifest.json')
+  //   })
+  // ]
 })
