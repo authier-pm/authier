096c63bbb7b2de4ebbd83a03f53d8bec4b379779
Jiří Špác <capajj@gmail.com>
2022-04-09 11:10:05 +0200

    wip

--- web-extension/src/content-script/components/Test.tsx
+++ web-extension/src/content-script/components/Test.tsx
@@ -1,10 +1,13 @@
 import { h } from 'nano-jsx/lib/core'
+import { css } from '@emotion/css'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const nano = h
 
 export const Test = () => {
   console.log('nanojsx')
   return (
     <div>
-      <h1>test</h1>
+      <h1>test3243</h1>
     </div>
   )
 }
