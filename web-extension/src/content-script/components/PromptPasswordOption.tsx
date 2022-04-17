7b65a6930d8af6a0718444559270cbd0ad83a491
sleaper <petrik.spac@gmail.com>
2022-04-17 10:18:24 +0200

    position fix on resize

--- web-extension/src/content-script/components/PromptPasswordOption.tsx
+++ web-extension/src/content-script/components/PromptPasswordOption.tsx
@@ -2,6 +2,8 @@
 import { h } from 'preact'
 import { ILoginSecret } from '../../util/useDeviceState'
 import { WebInputType } from '../../../../shared/generated/graphqlBaseTypes'
+import { useState, useEffect } from 'preact/hooks'
+import { promptOption } from '../renderLoginCredOption'
 //import { css } from '@emotion/css'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const nano = h
@@ -24,8 +26,17 @@ export const PromptPasswordOption = ({
   }>
 }) => {
   const el = document.querySelector(webInputs[0].domPath)
-  //TODO: resize
-  let elPosition = el?.getBoundingClientRect()
+  const [test, setTest] = useState(el?.getBoundingClientRect())
+
+  let resizeTimer
+  window.onresize = function () {
+    promptOption.remove()
+    clearTimeout(resizeTimer)
+    resizeTimer = setTimeout(function () {
+      setTest(el?.getBoundingClientRect())
+      document.body.appendChild(promptOption)
+    }, 100)
+  }
 
   return (
     <div
@@ -36,11 +47,10 @@ export const PromptPasswordOption = ({
         alignItems: 'baseline',
         fontFamily: 'sans-serif !important',
         position: 'fixed',
-        top: elPosition?.top + 'px',
-        left:
-          (elPosition?.left as number) + (elPosition?.width as number) + 'px',
-        right: elPosition?.right + 'px',
-        bottom: elPosition?.bottom + 'px',
+        top: test?.top + 'px',
+        left: (test?.left as number) + (test?.width as number) + 'px',
+        right: test?.right + 'px',
+        bottom: test?.bottom + 'px',
         margin: '5px'
       }}
     >
@@ -65,3 +75,51 @@ export const PromptPasswordOption = ({
     </div>
   )
 }
+
+// export class Test extends Component {
+//   constructor() {
+//     super()
+//     this.initState = el?.getBoundingClientRect()
+//   }
+
+//   render() {
+//     console.log('Test', props)
+
+//     return (
+//       <div
+//         class="dropdown"
+//         style={{
+//           zIndex: '2147483647', // max z-index according to stackoverflow
+//           justifyContent: 'center',
+//           alignItems: 'baseline',
+//           fontFamily: 'sans-serif !important',
+//           position: 'fixed',
+//           top: value?.top + 'px',
+//           left: (value?.left as number) + (value?.width as number) + 'px',
+//           right: value?.right + 'px',
+//           bottom: value?.bottom + 'px',
+//           margin: '5px'
+//         }}
+//       >
+//         <button className="dropbtn">Dropdown</button>
+//         <div className="dropdown-content">
+//           {loginCredentials.map((el) => (
+//             <a
+//               onClick={async () => {
+//                 autofill({
+//                   secretsForHost: { loginCredentials: [el], totpSecrets: [] },
+//                   autofillEnabled: true,
+//                   extensionDeviceReady: true,
+//                   saveLoginModalsState: undefined,
+//                   webInputs: webInputs
+//                 })
+//               }}
+//             >
+//               {el.loginCredentials.username}
+//             </a>
+//           ))}
+//         </div>
+//       </div>
+//     )
+//   }
+// }
