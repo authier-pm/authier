fad31eb06ae98e76efdbd8081101858f46fbefda
Jiří Špác <capajj@gmail.com>
2022-04-23 17:31:20 +0200

    use separate error link for frontend
    Former-commit-id: f2166a3ad9b9760072a76651065a9565bdbd8324 [formerly a8ac374197c14ae9e5b84d017d0caca1006465f8]
    Former-commit-id: d80bbd1f0d48e68bea320687bf57ddbbca0bcc46

--- frontend/graphql/apolloClient.ts
+++ frontend/graphql/apolloClient.ts
@@ -4,8 +4,12 @@ import {
   createHttpLink,
   InMemoryCache
 } from '@apollo/client'
-import { errorLink } from '../../shared/errorLink'
+
 import jwtDecode from 'jwt-decode'
+import { onError } from '@apollo/client/link/error'
+import { toastifyConfig } from '../../shared/toastifyConfig'
+import { toast } from 'react-toastify'
+import { print } from 'graphql'
 
 const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL
 
@@ -23,6 +27,26 @@ export const getUserFromToken = async () => {
 const httpLink = createHttpLink({
   uri: `${NEXT_PUBLIC_API_URL}/graphql`,
   credentials: 'include'
+})
+
+const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+  const isExtension = window.location.href.startsWith('chrome-extension')
+
+  toast.configure(toastifyConfig(isExtension ? undefined : 'bottom-right'))
+
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, path }) => {
+      console.error(
+        `[GraphQL error]: Message: ${message}, operation: ${operation.operationName}, Path: ${path}`
+      )
+      console.error('full operation: ', print(operation.query))
+    })
+
+    toast.error(graphQLErrors[0].message ?? 'There was API error.')
+  } else if (networkError) {
+    console.error(`[Network error]: ${networkError}`)
+    toast.error('There was network error.')
+  }
 })
 
 export const apolloClient = new ApolloClient({
