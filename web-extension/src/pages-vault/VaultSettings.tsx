dce254b44048c2fedb70e1c8aa450dab6745b0d6
sleaper <petrik.spac@gmail.com>
2022-06-04 22:05:35 +0200

    small tweaks
    Former-commit-id: de97a1c731eb0dc0f99c97c1a91a9f74650e53b9 [formerly f80b695e4385949751263df45f2bf5cd501642d7]
    Former-commit-id: ffe413a719f26808348afa5b646bab365bc13048

--- web-extension/src/pages-vault/VaultSettings.tsx
+++ web-extension/src/pages-vault/VaultSettings.tsx
@@ -35,25 +35,25 @@ const LinkItems: Array<LinkItemProps> = [
 
 const NavLink = ({ name, path, handleClick, url, selected }: Props) => {
   return (
-    <Box
-      verticalAlign="center"
-      px={2}
-      py={1}
-      borderRadius="lg"
-      role="group"
-      cursor="pointer"
-      fontSize={20}
-      bgColor={
-        selected.name === name
-          ? useColorModeValue('gray.100', 'gray.700')
-          : 'transparent'
-      }
-      onClick={() => handleClick()}
-    >
-      <Link as={RouterLink} to={url + path} style={{ textDecoration: 'none' }}>
+    <Link as={RouterLink} to={url + path} style={{ textDecoration: 'none' }}>
+      <Box
+        verticalAlign="center"
+        px={2}
+        py={1}
+        borderRadius="lg"
+        role="group"
+        cursor="pointer"
+        fontSize={20}
+        bgColor={
+          selected.name === name
+            ? useColorModeValue('gray.100', 'gray.700')
+            : 'transparent'
+        }
+        onClick={() => handleClick()}
+      >
         {name}
-      </Link>
-    </Box>
+      </Box>
+    </Link>
   )
 }
 
