31d3888c215c507d57983fdbddc75a1ab8454a58
Jiří Špác <capajj@gmail.com>
2022-06-04 14:14:49 +0200

    remove deplicit folder

--- models/types/EmailVerificationType.ts
+++
@@ -1,9 +0,0 @@
-import { registerEnumType } from 'type-graphql'
-
-export enum EmailVerificationTypeGQL {
-  PRIMARY = 'PRIMARY',
-  CONTACT = 'CONTACT'
-}
-registerEnumType(EmailVerificationTypeGQL, {
-  name: 'EmailVerificationType'
-})
