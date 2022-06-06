8d0c88992f14fdb9b7bd6e99afbfb2a176778255
sleaper <petrik.spac@gmail.com>
2022-06-06 20:44:32 +0200

    stripe on frontend

--- backend/stripe.tsx
+++ backend/stripe.tsx
@@ -1,5 +1,5 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_API_KEY as string, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: '2020-08-27'
 })
