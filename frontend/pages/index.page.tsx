5a41205c63bb4ec685dec18df737d840141b526d
Petr Spac <petrik.spac@gmail.com>
2022-06-18 11:25:15 +0200

    playing with UI

--- frontend/pages/index.page.tsx
+++ frontend/pages/index.page.tsx
@@ -70,9 +70,7 @@ export default function Home() {
       </Head>
 
       <Hero />
-
       <FeaturesSection />
-      <SimpleThreeColumns />
       <Testimonials />
     </Box>
   )
@@ -147,63 +145,6 @@ const Hero = () => {
           ></chakra.p>
         </GridItem>
       </Container>
-    </Flex>
-  )
-}
-
-interface FeatureProps {
-  title: string
-  text: string
-  icon: ReactElement
-}
-
-const Feature = ({ title, text, icon }: FeatureProps) => {
-  return (
-    <Stack>
-      <Flex
-        w={16}
-        h={16}
-        align={'center'}
-        justify={'center'}
-        color={'white'}
-        rounded={'full'}
-        bg={'gray.100'}
-        mb={1}
-      >
-        {icon}
-      </Flex>
-      <Text fontWeight={600}>{title}</Text>
-      <Text color={'gray.600'}>{text}</Text>
-    </Stack>
-  )
-}
-
-export function SimpleThreeColumns() {
-  return (
-    <Flex p={4} alignItems="center" justifyContent="space-between" minH="90vh">
-      <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
-        <Feature
-          icon={<Icon as={FcAssistant} w={10} h={10} />}
-          title={'Lifetime Support'}
-          text={
-            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
-          }
-        />
-        <Feature
-          icon={<Icon as={FcDonate} w={10} h={10} />}
-          title={'Unlimited Donations'}
-          text={
-            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
-          }
-        />
-        <Feature
-          icon={<Icon as={FcInTransit} w={10} h={10} />}
-          title={'Instant Delivery'}
-          text={
-            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore...'
-          }
-        />
-      </SimpleGrid>
     </Flex>
   )
 }
