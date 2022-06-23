ce7c6e63fae74677ce39dcbe0ac1c8fed1b38d3e
Jiří Špác <capajj@gmail.com>
2022-06-23 19:50:32 +0200

    'react/prop-types': 'off'
    Former-commit-id: ec76ec10c6f1f3ff82f05e79f2c09860b9c7cd8a [formerly 18244ec1e5ab7591f758ba3a5b08db5b05768fd1]
    Former-commit-id: 1b2e96ae872f1d084437d0a6d73c4c33ced74e70

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,8 @@ module.exports = {
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-    '@typescript-eslint/ban-ts-comment': 'off'
+    '@typescript-eslint/ban-ts-comment': 'off',
+    'react/prop-types': 'off'
   },
   settings: {
     react: {
