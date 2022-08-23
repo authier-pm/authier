6cccc3e93f4c6d4e17cbde8d7e5fbefc47074b82
Jiří Špác <capajj@gmail.com>
2022-08-23 12:40:28 +0200

    bump deps and svg loading support for webpack
    Former-commit-id: 33327037fd358507ac924a95c66116c2592db3a4
    Former-commit-id: 88d6d5af7adc57aaa3ad8d83b55544810bb79bad

--- web-extension/webpack.common.js
+++ web-extension/webpack.common.js
@@ -61,6 +61,11 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack']
       }
     ]
   },
