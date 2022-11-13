da0029da84b21a41f02aeae22fd653acc7d12605
Jiří Špác <capajj@gmail.com>
2022-11-13 17:31:36 +0100

    possible fix for deployment
    Former-commit-id: 81cef166517ae99ed495bc297727ab1a2c8d7e1c [formerly 81cef166517ae99ed495bc297727ab1a2c8d7e1c [formerly 2a5dc5b7f2837e8d205027efe2f4e6d50b5dab03]]
    Former-commit-id: 5a374826cde8f93a162b2b888ffc73574498cb87 [formerly c06b453225ee4f5ce2c8ba7c8ffd8d35ec7eedab]
    Former-commit-id: 94c3a1dda63ce5984aaee11f035bc5ed4fa36ebe

--- backend/scripts/copyToDist.js
+++ backend/scripts/copyToDist.js
@@ -38,7 +38,7 @@ const fs = require('fs/promises')
     ]
   })
 
-  await cpy(['../node_modules/.prisma/**'], './dist/node_modules', {
+  await cpy(['./node_modules/.prisma/**'], './dist', {
     parents: true,
     ignore: [
       '../node_modules/.prisma/client/libquery_engine-debian-openssl-1.1.x.so.node',
