c2af61a34ee21ec40120546fe4ca5be2f0590e44
Petr Spac <petrik.spac@gmail.com>
2022-12-19 18:30:09 +0100

    Monorepo fix after some time (#258)
    * ios fixes
    
    * rnx-kit
    
    * ios fix
    
    * android fix
    
    * ios icons
    
    * generateEncryptionKey moved to shared
    
    * CI fix
    
    * tests
    
    * clean Device tests
    
    * More device tests
    
    Former-commit-id: 3294a90204f9d09f0f15fe8edc95e38c8803866f [formerly 3294a90204f9d09f0f15fe8edc95e38c8803866f [formerly ac44513549421f7050f6ff4836c98392d4f4cdfb]]
    Former-commit-id: 6b002ee2060b672b7490a0ac84c1376f4a8c3a6b [formerly 63eebfb270f936a1e3bfd516254afab960ff470d]
    Former-commit-id: cae6d2aa16d15ca42e9c8afc772f3b5f02883c3a

--- backend/models/Device.spec.ts
+++ backend/models/Device.spec.ts
@@ -4,19 +4,32 @@ import { makeRegisterAccountInput } from '../schemas/RootResolver.spec'
 import { DecryptionChallengeApproved } from './DecryptionChallenge'
 import { User } from '.prisma/client'
 import faker from 'faker'
-import { beforeAll, describe, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { DeviceMutation, DeviceQuery } from './Device'
 
 describe('Device', () => {
   let user: User
 
+  const masterDeviceId = faker.datatype.uuid()
+  const slaveDeviceId = faker.datatype.uuid()
+  const masterDeviceName = faker.random.word()
+  const slaveDeviceName = faker.random.word()
+  const userId = faker.datatype.uuid()
+  const slaveDeviceChallenge = faker.datatype.number()
+
   const challenge: DecryptionChallengeApproved =
     new DecryptionChallengeApproved()
+  const deviceMutation = new DeviceMutation()
+  const deviceQuery = new DeviceQuery()
+
+  challenge.id = faker.datatype.number()
+  challenge.blockIp = false
   challenge.deviceId = faker.datatype.uuid()
   challenge.deviceName = faker.random.word()
-  const userId = faker.datatype.uuid()
+
   const input: RegisterNewAccountInput = makeRegisterAccountInput()
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     user = await prismaClient.user.create({
       data: {
         id: userId,
@@ -24,17 +37,291 @@ describe('Device', () => {
         addDeviceSecret: input.addDeviceSecret,
         addDeviceSecretEncrypted: input.addDeviceSecretEncrypted,
         encryptionSalt: input.encryptionSalt,
-        loginCredentialsLimit: 50,
-        TOTPlimit: 4,
+        loginCredentialsLimit: faker.datatype.number(20),
+        TOTPlimit: faker.datatype.number(5),
         deviceRecoveryCooldownMinutes: 960
+      }
+    })
+
+    //Master device
+    await prismaClient.device.create({
+      data: {
+        id: masterDeviceId,
+        name: faker.random.word(),
+        firebaseToken: faker.datatype.string(5),
+        firstIpAddress: faker.internet.ip(),
+        lastIpAddress: faker.internet.ip(),
+        platform: faker.random.word(),
+        userId: userId
+      }
+    })
+
+    //Slave device
+    await prismaClient.device.create({
+      data: {
+        id: slaveDeviceId,
+        name: faker.random.word(),
+        firebaseToken: faker.datatype.string(5),
+        firstIpAddress: faker.internet.ip(),
+        lastIpAddress: faker.internet.ip(),
+        platform: faker.random.word(),
+        userId: userId
+      }
+    })
+
+    await prismaClient.decryptionChallenge.create({
+      data: {
+        id: slaveDeviceChallenge,
+        userId: userId,
+        deviceId: slaveDeviceId,
+        deviceName: faker.random.word(),
+        ipAddress: faker.internet.ip()
       }
     })
   })
 
-  describe('logout', () => {
-    it.todo('should logout user')
-    it.todo(
-      'should create a new pre approved device decryption challenge when logging out from master device'
-    )
+  afterEach(async () => {
+    const deleteChallenges = prismaClient.decryptionChallenge.deleteMany()
+    const deleteSecrets = prismaClient.encryptedSecret.deleteMany()
+    const deleteDevices = prismaClient.device.deleteMany()
+    const deleteUser = prismaClient.user.deleteMany()
+
+    await prismaClient.$transaction([
+      deleteChallenges,
+      deleteSecrets,
+      deleteDevices,
+      deleteUser
+    ])
+
+    await prismaClient.$disconnect()
+  })
+
+  describe('logout', async () => {
+    deviceMutation.userId = userId
+
+    it('slave device logout', async () => {
+      //Device info
+      deviceMutation.id = slaveDeviceId
+      deviceMutation.name = slaveDeviceName
+
+      // Create mock objects
+      const fakeCtx = {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        reply: { setCookie: () => {}, clearCookie: () => {} },
+        request: { headers: {} },
+        prisma: prismaClient,
+        jwtPayload: { userId: userId },
+        masterDeviceId: masterDeviceId,
+        getIpAddress: () => faker.internet.ip()
+      } as any
+
+      const logout = await deviceMutation.logout(fakeCtx)
+
+      const challenges = await prismaClient.decryptionChallenge.findMany({
+        where: {
+          deviceId: slaveDeviceId,
+          approvedAt: null
+        }
+      })
+
+      expect(challenges).toHaveLength(0)
+
+      expect(logout).toMatchObject({
+        id: slaveDeviceId,
+        logoutAt: expect.any(Date),
+        userId: deviceMutation.userId
+      })
+    })
+
+    it('master device logout', async () => {
+      //Device info
+      deviceMutation.id = masterDeviceId
+      deviceMutation.name = masterDeviceName
+
+      // Create mock objects
+      const fakeCtx = {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        reply: { setCookie: () => {}, clearCookie: () => vi.fn() },
+        request: { headers: {} },
+        prisma: prismaClient,
+        jwtPayload: { userId: userId },
+        masterDeviceId: masterDeviceId,
+        getIpAddress: () => faker.internet.ip()
+      } as any
+
+      const logout = await deviceMutation.logout(fakeCtx)
+
+      const challenges = await prismaClient.decryptionChallenge.findMany({
+        where: {
+          deviceId: masterDeviceId
+        }
+      })
+
+      //FIX: How to test clearCookie??
+      const spy = vi.spyOn(fakeCtx.reply, 'clearCookie')
+      expect(spy.getMockName()).toEqual('clearCookie')
+      expect(challenges).toHaveLength(1)
+      expect(logout).toMatchObject({
+        id: deviceMutation.id,
+        logoutAt: expect.any(Date),
+        userId: deviceMutation.userId
+      })
+    })
+  })
+
+  describe('removeDevice', async () => {
+    deviceMutation.userId = userId
+
+    it('Should remove slave device from user and DB', async () => {
+      //Device info
+      deviceMutation.id = slaveDeviceId
+      deviceMutation.name = slaveDeviceName
+
+      // Create mock objects
+      const fakeCtx = {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        reply: { setCookie: () => {}, clearCookie: () => vi.fn() },
+        request: { headers: {} },
+        prisma: prismaClient,
+        jwtPayload: { userId: userId },
+        masterDeviceId: masterDeviceId,
+        getIpAddress: () => faker.internet.ip()
+      } as any
+
+      const wasRemoved = await deviceMutation.removeDevice(fakeCtx)
+
+      const device = await prismaClient.device.findMany({
+        where: {
+          id: slaveDeviceId
+        }
+      })
+
+      const challenges = await prismaClient.decryptionChallenge.findMany({
+        where: {
+          deviceId: slaveDeviceId
+        }
+      })
+
+      expect(wasRemoved).toBeTruthy()
+      expect(device).toHaveLength(0)
+      expect(challenges).toHaveLength(0)
+    })
+
+    it("Should show 'You cannot remove master device from list.'", async () => {
+      //Device info
+      deviceMutation.id = masterDeviceId
+      deviceMutation.name = masterDeviceName
+
+      // Create mock objects
+      const fakeCtx = {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        reply: { setCookie: () => {}, clearCookie: () => vi.fn() },
+        request: { headers: {} },
+        prisma: prismaClient,
+        jwtPayload: { userId: userId },
+        masterDeviceId: masterDeviceId,
+        getIpAddress: () => faker.internet.ip()
+      } as any
+
+      //WARNING: Solve the await hell
+      await expect(
+        async () => await deviceMutation.removeDevice(fakeCtx)
+      ).rejects.toThrow('You cannot remove master device from list.')
+    })
+  })
+
+  describe('rename', async () => {
+    it('Should rename device', async () => {
+      //Device info
+      deviceMutation.id = masterDeviceId
+      deviceMutation.userId = userId
+      deviceMutation.name = masterDeviceName
+      const newName = faker.random.word()
+
+      // Create mock objects
+      const fakeCtx = {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        reply: { setCookie: () => {}, clearCookie: () => vi.fn() },
+        request: { headers: {} },
+        prisma: prismaClient,
+        jwtPayload: { userId: userId },
+        masterDeviceId: masterDeviceId,
+        getIpAddress: () => faker.internet.ip()
+      } as any
+
+      const rename = await deviceMutation.rename(fakeCtx, newName)
+
+      expect(rename).toMatchObject({
+        name: newName,
+        id: masterDeviceId
+      })
+    })
+  })
+
+  describe('encryptedSecretsToSync', async () => {
+    //Device info
+    deviceQuery.lastSyncAt = faker.date.past(1)
+    deviceQuery.userId = userId
+
+    const testData: any = []
+
+    // Create mock objects
+    const fakeCtx = {
+      // eslint-disable-next-line @typescript-eslint/no-empty-function
+      reply: { setCookie: () => {}, clearCookie: () => vi.fn() },
+      request: { headers: {} },
+      prisma: prismaClient,
+      jwtPayload: { userId: userId },
+      masterDeviceId: masterDeviceId,
+      getIpAddress: () => faker.internet.ip()
+    } as any
+
+    it('No limit exceeded', async () => {
+      //Generate fake secrets for user
+
+      for (let i = 0; i < user.loginCredentialsLimit; i++) {
+        testData.push({
+          encrypted: faker.datatype.string(25),
+          kind: 'LOGIN_CREDENTIALS',
+          userId: userId,
+          version: 1
+        })
+      }
+
+      await prismaClient.encryptedSecret.createMany({
+        data: testData
+      })
+
+      const secrets = await deviceQuery.encryptedSecretsToSync(fakeCtx)
+
+      expect(secrets).toHaveLength(user.loginCredentialsLimit)
+    })
+
+    it("should show 'TOTP limit exceeded, remove TOTP secrets'", async () => {
+      //Generate fake secrets for user
+      const numOverLimit = faker.datatype.number(5)
+      //TODO: Make function for generating random secrets, revise this code and remove duplicate lines
+
+      for (let i = 0; i < user.TOTPlimit + numOverLimit; i++) {
+        testData.push({
+          encrypted: faker.datatype.string(25),
+          kind: 'TOTP',
+          userId: userId,
+          version: 1
+        })
+      }
+
+      await prismaClient.encryptedSecret.createMany({
+        data: testData
+      })
+
+      await expect(
+        async () => await deviceQuery.encryptedSecretsToSync(fakeCtx)
+      ).rejects.toThrow(
+        `TOTP limit exceeded, remove ${
+          testData.length - user.TOTPlimit
+        } TOTP secrets`
+      )
+    })
   })
 })
