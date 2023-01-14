c32a6a30a6c671fb3ca3dba3f68c21685fa2fe34
Petr Spac <petrik.spac@gmail.com>
2023-01-14 22:58:53 +0100

    Upgrade

---
+++ mobile-app/ios/authier/AppDelegate.mm
@@ -0,0 +1,40 @@
+#import <Firebase.h>
+#import "AppDelegate.h"
+#import "RNBootSplash.h"
+#import <CodePush/CodePush.h>
+
+#import <React/RCTBundleURLProvider.h>
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
+  self.moduleName = @"authier";
+  [super application:application didFinishLaunchingWithOptions:launchOptions];
+  [RNBootSplash initWithStoryboard:@"BootSplash" rootView:self.window.rootViewController.view];
+  return YES;
+}
+
+- (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
+{
+#if DEBUG
+  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
+#else
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+#endif
+}
+
+/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
+///
+/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
+/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
+/// @return: `true` if the `concurrentRoot` feature is enabled. Otherwise, it returns `false`.
+- (BOOL)concurrentRootEnabled
+{
+  return true;
+}
+
+@end
