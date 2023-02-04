b461d57f23187a6f82f13cd06c86585bdb37c4e4
Petr Špác <petrik.spac@gmail.com>
2023-02-04 22:35:09 +0100

    small fixes

--- web-extension/src/AuthLinkPage.tsx
+++ web-extension/src/AuthLinkPage.tsx
@@ -2,12 +2,12 @@ import { Box } from '@chakra-ui/layout'
 import { Center, Heading } from '@chakra-ui/react'
 import { Tooltip, IconButton } from '@chakra-ui/react'
 import { t, Trans } from '@lingui/macro'
-import React, { ReactElement } from 'react'
+import { ReactElement } from 'react'
 import { IoMdArchive } from 'react-icons/io'
 import browser from 'webextension-polyfill'
 
 export function openVaultTab(afterHash: string = '') {
-  browser.tabs.create({ url: `js/vault.html#${afterHash}` })
+  browser.tabs.create({ url: `vault.html#${afterHash}` })
 }
 
 export function AuthLinkPage(): ReactElement {
@@ -16,7 +16,7 @@ export function AuthLinkPage(): ReactElement {
       <Box width="315px" p={30}>
         <Center>
           <Heading size="sm">
-            <Trans>Open vault to login3 or sign up</Trans>
+            <Trans>Open vault to login or sign up</Trans>
           </Heading>
           <Tooltip label={t`Open vault`} aria-label={t`Open vault`}>
             <IconButton
