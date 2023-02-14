6eea640a5face996605fc115adf02a213fa4159a
Jiří Špác <capajj@gmail.com>
2023-02-14 01:13:13 +0100

    remove obsolete comments
    Former-commit-id: 4f831cfc537e49d0b56249a132270573a9a88a34 [formerly adb8ab8630eadb63e57df07ec9de8e8aa2af6bc7]
    Former-commit-id: c819da4affe4abadea54db60ab968257d84e47c0

--- web-extension/webpack.prod.js
+++ web-extension/webpack.prod.js
@@ -2,14 +2,10 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const webpack = require('webpack')
 const TerserPlugin = require('terser-webpack-plugin')
+
 module.exports = merge(common, {
   devtool: 'source-map',
   mode: 'production',
-  // optimization: {
-  //   minimize: false
-  // minimizer: [new TerserPlugin()]
-  // TODO whenever we try to minify, the output bundle cannot be parsed. We should investigate this and find out where the problem is. Happens with terser and without.
-  // },
   optimization: {
     minimizer: [new TerserPlugin()],
     runtimeChunk: false,
