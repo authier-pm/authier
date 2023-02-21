1f5b749ce768b79ffe888495c64ee3521b94fa4c
Jiří Špác <capajj@gmail.com>
2023-02-21 13:02:56 +0100

    enahnce AddItem
    cache query when using useMeExtensionQuery in AddItem

--- web-extension/src/pages-vault/AddItem.tsx
+++ web-extension/src/pages-vault/AddItem.tsx
@@ -4,7 +4,8 @@ import {
   Select,
   Box,
   Text,
-  Spinner
+  Spinner,
+  Link
 } from '@chakra-ui/react'
 import { useContext, useState } from 'react'
 
@@ -14,23 +15,25 @@ import { AddLogin } from '@src/components/vault/addItem/AddLogin'
 import { AddTOTP } from '@src/components/vault/addItem/AddTOTP'
 import { useMeExtensionQuery } from './AccountLimits.codegen'
 import { DeviceStateContext } from '@src/providers/DeviceStateProvider'
+import { Txt } from '@src/components/util/Txt'
+import { Trans } from '@lingui/macro'
+import { Link as RouterLink } from 'react-router-dom'
 
 export const AddItem = () => {
   type secretType = 'login' | 'totp'
   const [type, setType] = useState<secretType>('login')
   const { loginCredentials: LoginCredentials, TOTPSecrets } =
     useContext(DeviceStateContext)
-  const { data, loading } = useMeExtensionQuery({
-    fetchPolicy: 'network-only'
-  })
+  const { data, loading } = useMeExtensionQuery()
 
   const bg = useColorModeValue('white', 'gray.800')
 
   if (loading) {
     return <Spinner />
   }
-  const totpCond = data!.me.TOTPlimit <= TOTPSecrets.length
-  const pswCond = data!.me.loginCredentialsLimit <= LoginCredentials.length
+  const totpLimitReached = (data?.me.TOTPlimit ?? 0) <= TOTPSecrets.length
+  const pswLimitReached =
+    (data?.me.loginCredentialsLimit ?? 0) <= LoginCredentials.length
 
   return (
     <motion.div
@@ -53,6 +56,17 @@ export const AddItem = () => {
         alignItems={'center'}
         bg={bg}
       >
+        {totpLimitReached || pswLimitReached ? (
+          <Txt mt={10} fontSize={'lg'} color="yellow.600">
+            <Trans>
+              You have reached your account limit. Go to{' '}
+              <Link as={RouterLink} to="/account-limits">
+                {' '}
+                Account Limits to upgrade your account.
+              </Link>{' '}
+            </Trans>
+          </Txt>
+        ) : null}
         <Select
           onChange={(e) => setType(e.target.value as secretType)}
           value={type}
@@ -60,10 +74,10 @@ export const AddItem = () => {
           w={'50%'}
           mt={5}
         >
-          <option disabled={totpCond} value="totp">
+          <option disabled={totpLimitReached} value="totp">
             TOTP
           </option>
-          <option disabled={pswCond} value="login">
+          <option disabled={pswLimitReached} value="login">
             Login
           </option>
         </Select>
