7e4ab42d4e22b6f99f8de173270072c1e9a08f68
Jiří Špác <capajj@gmail.com>
2023-02-22 15:18:11 +0100

    remove VaultUnlockVerification from popup
    it was causing vault to try to render in popup
    
    
    Former-commit-id: 7187e38a8794108637750a539faa945b8e93c0b2
    Former-commit-id: 4a8ee2ac5dd6391f1bf6c75609b706a16bc62a10

--- web-extension/src/index.tsx
+++ web-extension/src/index.tsx
@@ -13,7 +13,7 @@ Sentry.init({
   dsn: 'https://528d6bfc04eb436faea6046afc419f56@o997539.ingest.sentry.io/5955889'
 })
 
-let popupRoot
+let popupRoot: ReactDOM.Root
 export const renderPopup = () => {
   console.log('renderPopup')
   popupRoot.render(
@@ -28,8 +28,10 @@ export const renderPopup = () => {
   )
 }
 browser.tabs.query({ active: true, currentWindow: true }).then(() => {
-  popupRoot = ReactDOM.createRoot(
-    document.getElementById('popup') as HTMLElement
-  )
-  renderPopup()
+  document.addEventListener('DOMContentLoaded', () => {
+    popupRoot = ReactDOM.createRoot(
+      document.getElementById('popup') as HTMLElement
+    )
+    renderPopup()
+  })
 })
