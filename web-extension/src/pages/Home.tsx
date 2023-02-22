ef378150d75f7d525a571dd2bb7de9297db1d128
Jiří Špác <capajj@gmail.com>
2023-02-22 19:31:21 +0100

    some enhancements for the popup

--- web-extension/src/pages/Home.tsx
+++ web-extension/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useContext, useState } from 'react'
+import { FunctionComponent, useContext, useState } from 'react'
 
 import {
   Box,
@@ -7,21 +7,19 @@ import {
   FormControl,
   FormLabel,
   Grid,
-  IconButton,
+  Input,
   Switch,
   useInterval
 } from '@chakra-ui/react'
-import browser from 'webextension-polyfill'
 import { authenticator } from 'otplib'
+import { TbWorld } from 'react-icons/tb'
 
 import { DeviceStateContext } from '@src/providers/DeviceStateProvider'
-import { AddIcon } from '@chakra-ui/icons'
 import { AuthsList } from '@src/components/pages/AuthsList'
-import { openVaultTab } from '@src/AuthLinkPage'
 
 export const Home: FunctionComponent = () => {
   const [seconds, setRemainingSeconds] = useState(authenticator.timeRemaining())
-
+  const [search, setSearch] = useState('')
   const { deviceState, TOTPSecrets, currentURL } =
     useContext(DeviceStateContext)
 
@@ -40,26 +38,34 @@ export const Home: FunctionComponent = () => {
     <>
       <Flex position="sticky" align="center" pl={4} pr={4} mt={'56px'}>
         <FormControl display="flex" alignItems="center">
-          <FormLabel mb="0">Filter by TLD</FormLabel>
-          <Switch
-            mr="auto"
-            isChecked={filterByTLD}
-            onChange={(enabled) => {
-              setFilterByTLD(enabled.target.checked)
-            }}
-          ></Switch>
-        </FormControl>
+          <Flex alignItems="center">
+            <FormLabel mb="0">
+              <Flex alignItems="center">
+                <TbWorld></TbWorld> TLD
+              </Flex>
+            </FormLabel>
+            <Switch
+              mr="auto"
+              isChecked={filterByTLD}
+              onChange={(enabled) => {
+                setFilterByTLD(enabled.target.checked)
+              }}
+            ></Switch>
 
-        <IconButton
-          mr={15}
-          colorScheme="blue"
-          aria-label="Add item"
-          icon={<AddIcon />}
-          rounded={'full'}
-          onClick={async () => {
-            openVaultTab('/addItem?url=' + currentURL)
-          }}
-        />
+            <Input
+              ml={5}
+              size="sm"
+              placeholder="Search"
+              onChange={(e) => {
+                setFilterByTLD(false)
+                if (e.target.value === '') {
+                  setFilterByTLD(true)
+                }
+                setSearch(e.target.value)
+              }}
+            />
+          </Flex>
+        </FormControl>
 
         {deviceState && TOTPSecrets.length > 0 && (
           <CircularProgress
@@ -74,7 +80,7 @@ export const Home: FunctionComponent = () => {
       </Flex>
       <Box height={300} width={350} pr={5} pl={5} mb={2}>
         <Grid gap={3} mb={5}>
-          <AuthsList filterByTLD={filterByTLD} />
+          <AuthsList filterByTLD={filterByTLD} search={search} />
         </Grid>
       </Box>
     </>
