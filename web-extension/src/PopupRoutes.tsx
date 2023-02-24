4f3539b957b42043b7ae5e9f64bfa6f49f4a2159
Jiri Spac <capajj@gmail.com>
2023-02-24 17:25:05 +0100

    make UnlockDeviceForm universal (#315)
    so that it can run in popup too
    
    Former-commit-id: 01125a1102c2fef30d6f89089bc302e147ec311a [formerly 01125a1102c2fef30d6f89089bc302e147ec311a [formerly fc9f463ded7d030bfdeebcf8a18f8f162222aba3]]
    Former-commit-id: a732020c979db1603c6c8434070bc56cef45474a [formerly 6df1a808a2a974123c0c64cecb89398b070599b9]
    Former-commit-id: b9f5ac78bcf8b16a3c8fb5d8fa03b273fbf8f64f

--- web-extension/src/PopupRoutes.tsx
+++ web-extension/src/PopupRoutes.tsx
@@ -2,17 +2,28 @@ import { ReactElement, useContext } from 'react'
 import { AuthLinkPage } from './AuthLinkPage'
 import { Popup } from './popup/Popup'
 import { DeviceStateContext } from './providers/DeviceStateProvider'
+import { UnlockDeviceForm } from './pages/UnlockDeviceForm'
 import debug from 'debug'
+import { useLocation } from 'wouter'
 const log = debug('au:popupRoutes')
 
 export default function PopupRoutes(): ReactElement {
   const { deviceState, lockedState } = useContext(DeviceStateContext)
 
+  const [, setLocation] = useLocation()
+
   log('deviceState', deviceState, lockedState)
 
-  // if (lockedState) { // TODO this would be nice, but we cannot reuse VaultUnlockVerification because it depends on react-router and stuff
-  //   return <VaultUnlockVerification />
-  // }
+  if (lockedState) {
+    // TODO this would be nice, but we cannot reuse VaultUnlockVerification because it depends on react-router and stuff
+    return (
+      <UnlockDeviceForm
+        onUnlocked={() => {
+          setLocation('/')
+        }}
+      />
+    )
+  }
 
   if (lockedState || deviceState === null) {
     return <AuthLinkPage />
