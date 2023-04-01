10e01f2ba793f7681a46a0192f10eea68b91e0c4
Petr Spac <petrik.spac@gmail.com>
2023-04-01 19:40:27 +0200

    Remove CodePush imports and configurations from the mobile app

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -14,25 +14,24 @@ import { Providers } from './src/Providers'
 import { device } from './src/utils/Device'
 import { useForceUpdate } from './useForceUpdate'
 import { Alert } from 'react-native'
-// import CodePush from 'react-native-code-push'
 import { queueLink } from './src/apollo/ApolloClient'
 import NetInfo from '@react-native-community/netinfo'
 import messaging from '@react-native-firebase/messaging'
 import * as Sentry from '@sentry/react-native'
 import PolyfillCrypto from 'react-native-webview-crypto'
-import CodePush from 'react-native-code-push'
+//import CodePush from 'react-native-code-push'
 import './src/sentryInit'
-
-let CodePushOptions = {
-  checkFrequency: __DEV__
-    ? CodePush.CheckFrequency.MANUAL
-    : CodePush.CheckFrequency.ON_APP_RESUME,
-  mandatoryInstallMode: CodePush.InstallMode.IMMEDIATE,
-  updateDialog: {
-    appendReleaseDescription: true,
-    title: 'a new update is available!'
-  }
-}
+//FIX: CodePush
+// let CodePushOptions = {
+//   checkFrequency: __DEV__
+//     ? CodePush.CheckFrequency.MANUAL
+//     : CodePush.CheckFrequency.ON_APP_RESUME,
+//   mandatoryInstallMode: CodePush.InstallMode.IMMEDIATE,
+//   updateDialog: {
+//     appendReleaseDescription: true,
+//     title: 'a new update is available!'
+//   }
+// }
 
 const App = () => {
   const forceUpdate = useForceUpdate()
@@ -78,4 +77,6 @@ const App = () => {
     </React.Fragment>
   )
 }
-export default Sentry.wrap(CodePush(CodePushOptions)(App))
+export default Sentry.wrap(App)
+//FIX: CodePush
+//export default Sentry.wrap(CodePush(CodePushOptions)(App))
--- mobile-app/android/app/src/main/java/com/authier/MainApplication.java
+++ mobile-app/android/app/src/main/java/com/authier/MainApplication.java
@@ -12,17 +12,19 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.facebook.react.bridge.JSIModulePackage;
 import com.swmansion.reanimated.ReanimatedJSIModulePackage;
-import com.microsoft.codepush.react.CodePush;
+//FIX: CodePush
+// import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
       new DefaultReactNativeHost(this) {
 
-        @Override
-        protected String getJSBundleFile() {
-            return CodePush.getJSBundleFile();
-        }
+        //FIX: CodePush
+        // @Override
+        // protected String getJSBundleFile() {
+        //     return CodePush.getJSBundleFile();
+        // }
         
         @Override
         public boolean getUseDeveloperSupport() {
