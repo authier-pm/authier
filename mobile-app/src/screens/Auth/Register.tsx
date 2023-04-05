5e11db2818ef31c0efeef10f6dddf389a2be5473
Jiří Špác <capajj@gmail.com>
2023-04-06 00:05:01 +0200

    share one state between login and register
    for easier switching between the two

--- mobile-app/src/screens/Auth/Register.tsx
+++ mobile-app/src/screens/Auth/Register.tsx
@@ -34,10 +34,7 @@ import {
   generateEncryptionKey
 } from '@src/utils/generateEncryptionKey'
 
-interface Values {
-  password: string
-  email: string
-}
+import { ILoginFormValues, LoginContext } from './Login'
 
 type NavigationProps = NativeStackScreenProps<AuthStackParamList, 'Register'>
 
@@ -47,6 +44,7 @@ export function Register({ navigation }: NavigationProps) {
   const toast = useToast()
   const id = 'active-toast'
   let device = useContext(DeviceContext)
+  const { formState, setFormState } = useContext(LoginContext)
 
   useEffect(() => {
     if (error) {
@@ -68,94 +66,94 @@ export function Register({ navigation }: NavigationProps) {
     return <Loading />
   }
 
+  const onSubmit = async (
+    values: ILoginFormValues,
+    { setSubmitting }: FormikHelpers<ILoginFormValues>
+  ) => {
+    const userId = uuid.v4()
+    //WARNING: Solve permission of uniqueID in google play
+    const deviceId = await getUniqueId()
+    const deviceName = await getDeviceName()
+
+    if (device.biometricsAvailable) {
+      await SInfo.setItem('psw', values.password, {
+        sharedPreferencesName: 'authierShared',
+        keychainService: 'authierKCH',
+        touchID: true,
+        showModal: true,
+        kSecAccessControl: 'kSecAccessControlBiometryAny'
+      })
+    }
+
+    const encryptionSalt = self.crypto.getRandomValues(new Uint8Array(16))
+
+    const masterEncryptionKey = await generateEncryptionKey(
+      values.password,
+      encryptionSalt
+    )
+
+    const params = await device.initLocalDeviceAuthSecret(
+      masterEncryptionKey,
+      encryptionSalt
+    )
+
+    const res = await register({
+      variables: {
+        userId: userId,
+        input: {
+          deviceId: deviceId,
+          ...params,
+          deviceName: deviceName,
+          email: values.email,
+          encryptionSalt: bufferToBase64(encryptionSalt),
+          firebaseToken: device.fireToken as string,
+          devicePlatform: Platform.OS
+        }
+      }
+    })
+
+    const registerResult = res.data?.registerNewUser
+
+    //FIX: Maybe we should check if is the access token valid?
+    if (registerResult?.accessToken) {
+      await saveAccessToken(registerResult.accessToken)
+      const stringKey = await cryptoKeyToString(masterEncryptionKey)
+
+      const newDeviceState: IBackgroundStateSerializable = {
+        masterEncryptionKey: stringKey,
+        userId: userId as string,
+        secrets: [],
+        email: values.email,
+        deviceName: device.name,
+        encryptionSalt: bufferToBase64(encryptionSalt),
+        authSecret: params.addDeviceSecret,
+        authSecretEncrypted: params.addDeviceSecretEncrypted,
+        lockTime: 28800,
+        autofillTOTPEnabled: false,
+        autofillCredentialsEnabled: false,
+        uiLanguage: 'en',
+        lockTimeEnd: Date.now() + 28800000,
+        syncTOTP: false,
+        theme: 'dark'
+      }
+
+      device.save(newDeviceState)
+      setSubmitting(false)
+    }
+    setSubmitting(false)
+  }
   return (
     <View p="8" justifyContent="center">
       <Heading size="lg" fontWeight="600">
         Welcome
       </Heading>
       <Heading mt="1" fontWeight="medium" size="xs">
-        Register!
+        Beware that authier is in beta, so we advise not to use it for important
+        passwords just yet. We will assess the security of the app before we
+        recommend it for important passwords.
       </Heading>
 
-      <Formik
-        initialValues={{ email: '', password: '' }}
-        onSubmit={async (
-          values: Values,
-          { setSubmitting }: FormikHelpers<Values>
-        ) => {
-          const userId = uuid.v4()
-          //WARNING: Solve permission of uniqueID in google play
-          const deviceId = await getUniqueId()
-          const deviceName = await getDeviceName()
-
-          if (device.biometricsAvailable) {
-            await SInfo.setItem('psw', values.password, {
-              sharedPreferencesName: 'authierShared',
-              keychainService: 'authierKCH',
-              touchID: true,
-              showModal: true,
-              kSecAccessControl: 'kSecAccessControlBiometryAny'
-            })
-          }
-
-          const encryptionSalt = self.crypto.getRandomValues(new Uint8Array(16))
-
-          const masterEncryptionKey = await generateEncryptionKey(
-            values.password,
-            encryptionSalt
-          )
-
-          const params = await device.initLocalDeviceAuthSecret(
-            masterEncryptionKey,
-            encryptionSalt
-          )
-
-          const res = await register({
-            variables: {
-              userId: userId,
-              input: {
-                deviceId: deviceId,
-                ...params,
-                deviceName: deviceName,
-                email: values.email,
-                encryptionSalt: bufferToBase64(encryptionSalt),
-                firebaseToken: device.fireToken as string,
-                devicePlatform: Platform.OS
-              }
-            }
-          })
-
-          const registerResult = res.data?.registerNewUser
-
-          //FIX: Maybe we should check if is the access token valid?
-          if (registerResult?.accessToken) {
-            await saveAccessToken(registerResult.accessToken)
-            const stringKey = await cryptoKeyToString(masterEncryptionKey)
-
-            const newDeviceState: IBackgroundStateSerializable = {
-              masterEncryptionKey: stringKey,
-              userId: userId as string,
-              secrets: [],
-              email: values.email,
-              deviceName: device.name,
-              encryptionSalt: bufferToBase64(encryptionSalt),
-              authSecret: params.addDeviceSecret,
-              authSecretEncrypted: params.addDeviceSecretEncrypted,
-              lockTime: 28800,
-              autofillTOTPEnabled: false,
-              autofillCredentialsEnabled: false,
-              uiLanguage: 'en',
-              lockTimeEnd: Date.now() + 28800000,
-              syncTOTP: false,
-              theme: 'dark'
-            }
-
-            device.save(newDeviceState)
-            setSubmitting(false)
-          }
-          setSubmitting(false)
-        }}
-      >
+      <Formik initialValues={formState} enableReinitialize onSubmit={onSubmit}>
         {({
           handleChange,
           handleBlur,
@@ -163,70 +161,74 @@ export function Register({ navigation }: NavigationProps) {
           values,
           errors,
           isSubmitting
-        }) => (
-          <VStack space={3} mt="5">
-            <FormControl>
-              <FormControl.Label
-                _text={{
-                  fontSize: 'xl',
-                  fontWeight: 500
-                }}
+        }) => {
+          setFormState(values)
+
+          return (
+            <VStack space={3} mt="5">
+              <FormControl>
+                <FormControl.Label
+                  _text={{
+                    fontSize: 'xl',
+                    fontWeight: 500
+                  }}
+                >
+                  Email
+                </FormControl.Label>
+
+                <Input
+                  onBlur={handleBlur('email')}
+                  onChangeText={handleChange('email')}
+                  value={values.email}
+                />
+                <FormControl.ErrorMessage>
+                  {errors.email}
+                </FormControl.ErrorMessage>
+              </FormControl>
+
+              <FormControl>
+                <FormControl.Label
+                  _text={{
+                    fontSize: 'xl',
+                    fontWeight: 500
+                  }}
+                >
+                  Password
+                </FormControl.Label>
+                <Input
+                  onChangeText={handleChange('password')}
+                  onBlur={handleBlur('password')}
+                  value={values.password}
+                  type={show ? 'text' : 'password'}
+                  InputRightElement={
+                    <Icon
+                      as={
+                        <Ionicons
+                          name={show ? 'eye-outline' : 'eye-off-outline'}
+                        />
+                      }
+                      size={5}
+                      mr="2"
+                      color="muted.400"
+                      onPress={() => setShow(!show)}
+                    />
+                  }
+                  placeholder="Password"
+                />
+                <FormControl.ErrorMessage>
+                  {errors.password}
+                </FormControl.ErrorMessage>
+              </FormControl>
+
+              <Button
+                onPress={handleSubmit as (values: any) => void}
+                isLoading={isSubmitting}
               >
-                Email
-              </FormControl.Label>
-
-              <Input
-                onBlur={handleBlur('email')}
-                onChangeText={handleChange('email')}
-                value={values.email}
-              />
-              <FormControl.ErrorMessage>
-                {errors.email}
-              </FormControl.ErrorMessage>
-            </FormControl>
-
-            <FormControl>
-              <FormControl.Label
-                _text={{
-                  fontSize: 'xl',
-                  fontWeight: 500
-                }}
-              >
-                Password
-              </FormControl.Label>
-              <Input
-                onChangeText={handleChange('password')}
-                onBlur={handleBlur('password')}
-                value={values.password}
-                type={show ? 'text' : 'password'}
-                InputRightElement={
-                  <Icon
-                    as={
-                      <Ionicons
-                        name={show ? 'eye-outline' : 'eye-off-outline'}
-                      />
-                    }
-                    size={5}
-                    mr="2"
-                    color="muted.400"
-                    onPress={() => setShow(!show)}
-                  />
-                }
-                placeholder="Password"
-              />
-              <FormControl.ErrorMessage>
-                {errors.password}
-              </FormControl.ErrorMessage>
-            </FormControl>
-
-            <Button
-              onPress={handleSubmit as (values: any) => void}
-              isLoading={isSubmitting}
-            >
-              Register
-            </Button>
-          </VStack>
-        )}
+                Register
+              </Button>
+            </VStack>
+          )
+        }}
       </Formik>
       <HStack mt="2" justifyContent="center" space={1}>
         <Text fontSize="sm" color="muted.700" fontWeight={400}>
--- mobile-app/src/sentryInit.ts
+++ mobile-app/src/sentryInit.ts
@@ -5,15 +5,17 @@ export const routingInstrumentation =
   new Sentry.ReactNavigationInstrumentation()
 
 // TODO reenable when we get the proper DSN
-Sentry.init({
-  dsn: SENTRY_DSN,
-  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-  // We recommend adjusting this value in production.
-  tracesSampleRate: 1.0,
-  integrations: [
-    new Sentry.ReactNativeTracing({
-      // Pass instrumentation to be used as `routingInstrumentation`
-      routingInstrumentation
-    })
-  ]
-})
+if (!__DEV__) {
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+    // We recommend adjusting this value in production.
+    tracesSampleRate: 1.0,
+    integrations: [
+      new Sentry.ReactNativeTracing({
+        // Pass instrumentation to be used as `routingInstrumentation`
+        routingInstrumentation
+      })
+    ]
+  })
+}
