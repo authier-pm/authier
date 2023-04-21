1d6421c8a205a21d89f8cccc255f35b58f085ba0
Jiří Špác <capajj@gmail.com>
2023-04-21 10:19:00 +0200

    fix TSC

--- mobile-app/__tests__/App-test.tsx
+++ mobile-app/__tests__/App-test.tsx
@@ -6,6 +6,7 @@ import React from 'react'
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
+import { it } from '@jest/globals'
 
 it('renders correctly', () => {
   renderer.create(<></>)
