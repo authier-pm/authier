923f20f47c454d6963430a09c33edc5893adaa70
Petr Spac <petrik.spac@gmail.com>
2023-05-24 14:52:24 +0200

    fix test

--- mobile-app/src/utils/Device.spec.tsx
+++ mobile-app/src/utils/Device.spec.tsx
@@ -3,81 +3,82 @@
 // import { device, Device } from '../utils/Device'
 // import { getSensitiveItem } from './secretStorage'
 
-// jest.mock('@react-native-firebase/messaging', () => {
-//   return {
-//     __esModule: true,
-//     default: () => ({
-//       getToken: jest.fn(() => Promise.resolve('test_token'))
-//     })
-//   }
-// })
+jest.mock('@react-native-firebase/messaging', () => {
+  return {
+    __esModule: true,
+    default: () => ({
+      getToken: jest.fn(() => Promise.resolve('test_token'))
+    })
+  }
+})
 
-// jest.mock('react-native-device-info', () => {
-//   return {
-//     getUniqueId: jest.fn(() => Promise.resolve('test_device_id')),
-//     getDeviceName: jest.fn(() => Promise.resolve('test_device_name'))
-//   }
-// })
+jest.mock('react-native-device-info', () => {
+  return {
+    getUniqueId: jest.fn(() => Promise.resolve('test_device_id')),
+    getDeviceName: jest.fn(() => Promise.resolve('test_device_name'))
+  }
+})
 
-// jest.mock('../utils/secretStorage.ts', () => {
-//   return {
-//     getSensitiveItem: jest.fn(() => Promise.resolve(null)),
-//     setSensitiveItem: jest.fn(() => Promise.resolve(null))
-//   }
-// })
+jest.mock('../utils/secretStorage.ts', () => {
+  return {
+    getSensitiveItem: jest.fn(() => Promise.resolve(null)),
+    setSensitiveItem: jest.fn(() => Promise.resolve(null))
+  }
+})
 
-// jest.mock('react-native-sensitive-info', () => ({
-//   setItem: jest.fn().mockResolvedValue(true),
-//   getItem: jest.fn().mockResolvedValue(null)
+jest.mock('react-native-sensitive-info', () => ({
+  setItem: jest.fn().mockResolvedValue(true),
+  getItem: jest.fn().mockResolvedValue(null)
+}))
+
+// jest.mock('@apollo/client/link/error', () => ({
+//   onError: jest.fn()
 // }))
-
-// // jest.mock('@apollo/client/link/error', () => ({
-// //   onError: jest.fn()
-// // }))
-// //
-// // jest.mock('@apollo/client', () => ({
-// //   ApolloClient: jest.fn(),
-// //   InMemoryCache: jest.fn(),
-// //   from: jest.fn(),
-// //   HttpLink: jest.fn(),
-// //   ApolloLink: jest.fn(),
-// //   setContext: jest.fn()
-// // }))
-// //
-// // jest.mock('apollo-link-retry', () => ({
-// //   RetryLink: jest.fn()
-// // }))
-// //
-// // jest.mock('apollo-link-serialize', () => jest.fn())
-// //
-// // jest.mock('apollo-link-queue', () => jest.fn())
-
-// jest.mock('../utils/tokenFromAsyncStorage', () => ({
-//   accessToken: 'dummy_token'
+//
+// jest.mock('@apollo/client', () => ({
+//   ApolloClient: jest.fn(),
+//   InMemoryCache: jest.fn(),
+//   from: jest.fn(),
+//   HttpLink: jest.fn(),
+//   ApolloLink: jest.fn(),
+//   setContext: jest.fn()
 // }))
+//
+// jest.mock('apollo-link-retry', () => ({
+//   RetryLink: jest.fn()
+// }))
+//
+// jest.mock('apollo-link-serialize', () => jest.fn())
+//
+// jest.mock('apollo-link-queue', () => jest.fn())
 
-// // jest.mock('../apollo/tokenRefresh.ts', () => jest.fn())
+jest.mock('../utils/tokenFromAsyncStorage', () => ({
+  accessToken: 'dummy_token'
+}))
 
-// // describe('Device', () => {
-// //   let device: Device
+// jest.mock('../apollo/tokenRefresh.ts', () => jest.fn())
 
-// //   beforeEach(() => {
-// //     device = new Device()
-// //     jest.clearAllMocks()
-// //   })
+describe('Device', () => {
+  it.todo('should initialize correctly')
+  // let device: Device
 
-// //   describe('initialize', () => {
-// //     it('should initialize correctly', async () => {
-// //       await device.initialize()
+  //  beforeEach(() => {
+  //    device = new Device()
+  //    jest.clearAllMocks()
+  //  })
 
-// //       expect(getUniqueId).toHaveBeenCalledTimes(1)
-// //       expect(getDeviceName).toHaveBeenCalledTimes(1)
-// //       expect(messaging().getToken).toHaveBeenCalledTimes(1)
-// //       expect(getSensitiveItem).toHaveBeenCalledWith('deviceState')
-// //       expect(device.isInitialized).toBe(true)
-// //       expect(device.id).toBe('test_device_id')
-// //       expect(device.name).toBe('test_device_name')
-// //       expect(device.fireToken).toBe('test_token')
-// //     })
-// //   })
-// // })
+  //  describe('initialize', () => {
+  //    it('should initialize correctly', async () => {
+  //      await device.initialize()
+
+  //      expect(getUniqueId).toHaveBeenCalledTimes(1)
+  //      expect(getDeviceName).toHaveBeenCalledTimes(1)
+  //      expect(messaging().getToken).toHaveBeenCalledTimes(1)
+  //      expect(getSensitiveItem).toHaveBeenCalledWith('deviceState')
+  //      expect(device.isInitialized).toBe(true)
+  //      expect(device.id).toBe('test_device_id')
+  //      expect(device.name).toBe('test_device_name')
+  //      expect(device.fireToken).toBe('test_token')
+  //    })
+  //  })
+})
