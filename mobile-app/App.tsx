a6d5d74febc72678b197bd4e4de00de19257f5e2
Petr Špác <petrik.spac@gmail.com>
2023-06-05 13:17:48 +0200

    fix: android only code

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -47,7 +47,6 @@ const RnApp = () => {
   }
 
   useEffect(() => {
-    // initializeStorage()
     requestUserPermission()
     device.initialize()
 
--- mobile-app/ios/authier/AppDelegate.mm
+++ mobile-app/ios/authier/AppDelegate.mm
@@ -1,6 +1,6 @@
 #import "AppDelegate.h"
-#import <Firebase.h>
 #import "RNBootSplash.h"
+#import <Firebase.h>
 #import <CodePush/CodePush.h>
 
 #import <React/RCTBundleURLProvider.h>
@@ -13,13 +13,9 @@
 
   self.moduleName = @"authier";
   // You can add your custom initial props in the dictionary below.
-  // They will be passed down to the ViewController used by React Native.
-  self.initialProps = @{};
-  [super application:application didFinishLaunchingWithOptions:launchOptions];
-
-  [RNBootSplash initWithStoryboard:@"BootSplash" rootView:self.window.rootViewController.view]; // <- initialization using the storyboard file name
-
-  return YES;
+    // They will be passed down to the ViewController used by React Native.
+    self.initialProps = @{};
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
@@ -39,6 +35,18 @@
 - (BOOL)concurrentRootEnabled
 {
   return true;
+}
+
+- (UIView *)createRootViewWithBridge:(RCTBridge *)bridge
+                          moduleName:(NSString *)moduleName
+                           initProps:(NSDictionary *)initProps {
+  UIView *rootView = [super createRootViewWithBridge:bridge
+                                          moduleName:moduleName
+                                           initProps:initProps];
+
+  [RNBootSplash initWithStoryboard:@"BootSplash" rootView:rootView];
+
+  return rootView;
 }
 
 @end
