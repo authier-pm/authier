cd80b852aed4919bd67d0fb1b62fd25beb64a0b6
Jiří Špác <capajj@gmail.com>
2023-06-15 18:28:53 +0200

    add redis url log

--- backend/lib/redisClient.ts
+++ backend/lib/redisClient.ts
@@ -1,5 +1,7 @@
 import Redis from 'ioredis'
 
+console.log('process.env.REDIS_URL :', process.env.REDIS_URL)
+
 export const redisClient = new Redis(process.env.REDIS_URL as string, {
   enableAutoPipelining: true
 })
