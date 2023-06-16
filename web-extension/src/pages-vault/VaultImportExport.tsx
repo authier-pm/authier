0c934307db635782aebfbe4ed310534bd3d90903
Jiří Špác <capajj@gmail.com>
2023-06-16 12:24:18 +0200

    add missing indexes and more tweaks in WE

--- web-extension/src/pages-vault/VaultImportExport.tsx
+++ web-extension/src/pages-vault/VaultImportExport.tsx
@@ -326,7 +326,7 @@ export const VaultImportExport = () => {
                   setDuplicates([])
                 }}
               >
-                Merge now
+                Delete duplicates now
               </Button>
             </Flex>
           ) : (
