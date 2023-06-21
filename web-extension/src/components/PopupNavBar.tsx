7d66c5058f2283a68327dfb0635d03ea1a4604d6
Jiri Spac <capajj@gmail.com>
2023-06-21 23:29:56 +0200

    tweak routing and some UX polish (#410)

--- web-extension/src/components/PopupNavBar.tsx
+++ web-extension/src/components/PopupNavBar.tsx
@@ -16,19 +16,20 @@ import {
 import { HamburgerIcon, CloseIcon, LockIcon, AddIcon } from '@chakra-ui/icons'
 
 import { Link, useRoute, useLocation, LinkProps, LocationHook } from 'wouter'
-import { NavMenu } from '@src/pages/NavMenu'
+
 import { UserNavMenu } from '@src/pages/UserNavMenu'
 import { IoMdArchive } from 'react-icons/io'
 import { t } from '@lingui/macro'
 import { RefreshSecretsButton } from './RefreshSecretsButton'
 import { openVaultTab } from '@src/AuthLinkPage'
 import { DeviceStateContext } from '@src/providers/DeviceStateProvider'
+import { AddSecretNavMenu } from '@src/pages/AddSecretNavMenu'
 
 export const PopupNavBar: FunctionComponent = () => {
   const {
-    isOpen: isNavMenuOpen,
-    onOpen: onNavMenuOpen,
-    onClose: onNavMenuClose
+    isOpen: isAddSecretNavMenuOpen,
+    onOpen: onAddSecretNavMenuOpen,
+    onClose: onAddSecretNavMenuClose
   } = useDisclosure()
   const {
     isOpen: isUserMenuOpen,
@@ -37,19 +38,6 @@ export const PopupNavBar: FunctionComponent = () => {
   } = useDisclosure()
   const [location, setLocation] = useLocation()
   const [lastPage, SetLastPage] = useState<string>('/')
-  const { currentURL } = useContext(DeviceStateContext)
-  const ActiveLink = (
-    props: JSX.IntrinsicAttributes &
-      React.PropsWithChildren<LinkProps<LocationHook>>
-  ) => {
-    const [isActive] = useRoute(props.href as string)
-
-    return (
-      <Link {...props}>
-        <a className={isActive ? 'active' : ''}>{props.children}</a>
-      </Link>
-    )
-  }
 
   const bg = useColorModeValue('gray.100', 'gray.800')
 
@@ -67,77 +55,69 @@ export const PopupNavBar: FunctionComponent = () => {
         borderBottomColor="gray.300"
         width="100%"
       >
-        <Box mr={2}>
-          {isUserMenuOpen ? (
-            <IconButton
-              size="md"
-              aria-label="menu"
-              icon={<CloseIcon />}
-              onClick={() => {
-                onUserMenuClose()
-              }}
-            />
-          ) : (
-            <IconButton
-              size="md"
-              aria-label="menu"
-              icon={<LockIcon />}
-              onClick={() => {
-                onUserMenuOpen()
-                onNavMenuClose()
-              }}
-            />
-          )}
-        </Box>
+        <Flex justify="space-between" minW={205}>
+          <Box mr={2}>
+            {isUserMenuOpen ? (
+              <IconButton
+                size="md"
+                aria-label="menu"
+                icon={<CloseIcon />}
+                onClick={() => {
+                  onUserMenuClose()
+                }}
+              />
+            ) : (
+              <IconButton
+                size="md"
+                aria-label="menu"
+                icon={<LockIcon />}
+                onClick={() => {
+                  onUserMenuOpen()
+                  onAddSecretNavMenuClose()
+                }}
+              />
+            )}
+          </Box>
 
-        <RefreshSecretsButton />
-        <Tooltip label={t`Open vault`} aria-label={t`Open vault`} mr={4}>
-          <IconButton
-            size="md"
-            ml="2"
-            aria-label="menu"
-            icon={<IoMdArchive />}
-            onClick={async () => {
-              openVaultTab()
-            }}
-          />
-        </Tooltip>
-        <IconButton
-          mr={15}
-          ml="2"
-          colorScheme="blue"
-          aria-label="Add item"
-          icon={<AddIcon />}
-          rounded={'full'}
-          onClick={async () => {
-            openVaultTab('/addItem?url=' + currentURL)
-          }}
-        />
-        <Box ml="auto">
-          {isNavMenuOpen ? (
+          <RefreshSecretsButton />
+
+          <Tooltip
+            label={isAddSecretNavMenuOpen ? t`close menu` : t`add secret`}
+            aria-label={isAddSecretNavMenuOpen ? t`close menu` : t`add secret`}
+          >
             <IconButton
-              size="md"
-              aria-label="menu"
-              icon={<CloseIcon />}
-              onClick={() => {
-                onNavMenuClose()
-              }}
-            />
-          ) : (
-            <IconButton
-              size="md"
-              aria-label="menu"
-              icon={<HamburgerIcon />}
-              onClick={() => {
-                onNavMenuOpen()
+              mr={15}
+              ml="2"
+              colorScheme="blue"
+              aria-label="Add item"
+              icon={isAddSecretNavMenuOpen ? <CloseIcon /> : <AddIcon />}
+              rounded={'full'}
+              onClick={async () => {
+                !isAddSecretNavMenuOpen
+                  ? onAddSecretNavMenuOpen()
+                  : onAddSecretNavMenuClose()
                 onUserMenuClose()
               }}
             />
-          )}
+          </Tooltip>
+        </Flex>
+
+        <Box ml="auto">
+          <Tooltip label={t`Open vault`} aria-label={t`Open vault`} mr={4}>
+            <IconButton
+              size="md"
+              ml="2"
+              aria-label="menu"
+              icon={<IoMdArchive />}
+              onClick={async () => {
+                openVaultTab()
+              }}
+            />
+          </Tooltip>
         </Box>
       </Flex>
 
-      {isNavMenuOpen && <NavMenu />}
+      {isAddSecretNavMenuOpen && <AddSecretNavMenu />}
       {isUserMenuOpen && <UserNavMenu />}
     </Flex>
   )
--- web-extension/src/pages/AddSecretNavMenu.tsx
+++ web-extension/src/pages/AddSecretNavMenu.tsx
@@ -1,38 +1,31 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useContext } from 'react'
 
 import { Button, ButtonGroup, Stack, useColorModeValue } from '@chakra-ui/react'
 
-import { InfoOutlineIcon } from '@chakra-ui/icons'
+import { AddIcon, InfoOutlineIcon } from '@chakra-ui/icons'
 import { Link } from 'wouter'
 import { AddTOTPSecretButton } from '@src/components/pages/AddTOTPSecretButton'
 import { openVaultTab } from '@src/AuthLinkPage'
+import { DeviceStateContext } from '@src/providers/DeviceStateProvider'
 
-export const NavMenu: FunctionComponent = () => {
+export const AddSecretNavMenu: FunctionComponent = () => {
   const bg = useColorModeValue('teal.200', 'teal.700')
+  const { currentURL } = useContext(DeviceStateContext)
 
   return (
     <Stack direction="row" bgColor={bg} justify="center" p="10px">
       <ButtonGroup spacing={4}>
         <Stack>
-          <Link to="/">
-            <Button colorScheme="teal">Secrets</Button>
-          </Link>
-          <AddTOTPSecretButton />
-        </Stack>
-      </ButtonGroup>
-      <ButtonGroup spacing={4} variant="solid" m="10px">
-        <Stack direction="column">
-          <Link to="/about">
-            <Button leftIcon={<InfoOutlineIcon />}>About</Button>
-          </Link>
-
           <Button
+            colorScheme="teal"
+            leftIcon={<AddIcon />}
             onClick={() => {
-              openVaultTab()
+              openVaultTab('/addItem?url=' + currentURL)
             }}
           >
-            My vault
+            Add manually
           </Button>
+          <AddTOTPSecretButton />
         </Stack>
       </ButtonGroup>
     </Stack>
