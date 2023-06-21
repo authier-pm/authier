895106e114ecd3dec968c9a6688e5b96f6e7c48e
Jiří Špác <capajj@gmail.com>
2023-06-21 12:21:40 +0200

    bump backend deps

--- backend/lib/redisClient.ts
+++ backend/lib/redisClient.ts
@@ -1,15 +1,8 @@
 import { Redis } from '@upstash/redis'
 
-console.log(
-  'process.env.UPSTASH_REDIS_REST_URL :',
-  process.env.UPSTASH_REDIS_REST_URL
-)
-
 const env = process.env
 
 export const redisClient = new Redis({
   url: env.UPSTASH_REDIS_REST_URL as string,
   token: env.UPSTASH_REDIS_REST_TOKEN as string
 })
-
-redisClient.ping().then((res) => console.log('redis', res))
