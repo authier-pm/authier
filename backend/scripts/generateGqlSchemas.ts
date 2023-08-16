1a4e833ba3f4fba7d8f4fbfdd50fd4f168728e30
Jiří Špác <capajj@gmail.com>
2023-08-16 12:43:41 +0200

    bump deps

--- backend/scripts/generateGqlSchemas.ts
+++ backend/scripts/generateGqlSchemas.ts
@@ -1,13 +1,13 @@
 import 'dotenv/config'
 import { printSchema } from 'graphql'
-import fs from 'mz/fs'
+import fs from 'fs/promises'
 import prettier from 'prettier'
 
 import { gqlSchema } from '../schemas/gqlSchema'
 ;(async () => {
   await fs.writeFile(
     './gqlSchemas/authier.graphql',
-    prettier.format(printSchema(gqlSchema), { parser: 'graphql' })
+    await prettier.format(printSchema(gqlSchema), { parser: 'graphql' })
   )
   console.log('✅ backend schema written')
 })()
