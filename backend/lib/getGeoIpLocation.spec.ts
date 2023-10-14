a6d0a58a7f53ec0292984a91da957d2dc983ccd3
Jiří Špác <capajj@gmail.com>
2023-10-14 20:10:31 +0200

    increase timeout for getGeoIpLocation spec

--- backend/lib/getGeoIpLocation.spec.ts
+++ backend/lib/getGeoIpLocation.spec.ts
@@ -1,9 +1,15 @@
 import { describe, expect, it } from 'vitest'
 import { getGeoIpLocation } from './getGeoIpLocation'
 
-describe('getGeoIpLocation', async () => {
-  it('should return a location', async () => {
-    const res = await getGeoIpLocation.memoized('89.176.72.144')
-    expect(res.country_name).toEqual('Czechia')
-  })
-})
+describe(
+  'getGeoIpLocation',
+  async () => {
+    it('should return a location', async () => {
+      const res = await getGeoIpLocation.memoized('89.176.72.144')
+      expect(res.country_name).toEqual('Czechia')
+    })
+  },
+  {
+    timeout: 10000
+  }
+)
