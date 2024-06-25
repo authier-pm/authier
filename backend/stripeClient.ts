7309f44df97dbe76e5c74bcfca3bd709bdb3d48e
Jiri Spac <capajj@gmail.com>
2024-06-25 23:43:42 +0200

    bump deps

--- backend/stripeClient.ts
+++ backend/stripeClient.ts
@@ -3,6 +3,6 @@ import Stripe from 'stripe'
 export const stripeClient = new Stripe(
   process.env.STRIPE_SECRET_KEY as string,
   {
-    apiVersion: '2024-04-10'
+    apiVersion: '2024-06-20'
   }
 )
