bbc88211cfd895c07cdb0b93adb2154b5890dc58
Jiri Spac <capajj@gmail.com>
2025-01-30 16:10:30 +0100

    add 1min wait

--- web-extension/scripts/release.ts
+++ web-extension/scripts/release.ts
@@ -1,27 +1,19 @@
 import fs from 'fs-extra'
-import { exec } from 'child_process'
+import { execSync } from 'child_process'
 import type PkgType from '../package.json'
 import { dir } from './generateExtensionManifest'
 
 async function pushNewTag() {
   const pkg = (await fs.readJSON(dir('package.json'))) as typeof PkgType
   const { version } = pkg
-
   const tagName = `v${version}-extension`
-  exec(
-    `git commit -m "Release version ${version}" && git tag -a ${tagName} -m "version ${version}" && git push origin`,
-    (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Failed to commit, create, or push git tag: ${error}`)
-        return
-      }
-      if (stderr) {
-        console.error(`stderr: ${stderr}`)
-        return
-      }
-      console.log(stdout)
-    }
-  )
+
+  execSync(`git commit -m "Release version ${version}"`)
+  execSync(`git tag -a ${tagName} -m "version ${version}"`)
+  execSync('git push origin')
+  execSync(`git push origin ${tagName}`)
+
+  console.log(`Successfully created and pushed tag ${tagName}`)
 }
 
 pushNewTag()
