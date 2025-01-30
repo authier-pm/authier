8e8e150f8533a8cc9d7f50caf634375d0acce0ec
Jiri Spac <capajj@gmail.com>
2025-01-30 14:42:20 +0100

    add git commit to release

--- web-extension/scripts/release.ts
+++ web-extension/scripts/release.ts
@@ -1,7 +1,7 @@
 import fs from 'fs-extra'
 import { exec } from 'child_process'
 import type PkgType from '../package.json'
-import { dir } from '../scripts/generateExtensionManifest'
+import { dir } from './generateExtensionManifest'
 
 async function pushNewTag() {
   const pkg = (await fs.readJSON(dir('package.json'))) as typeof PkgType
@@ -9,10 +9,10 @@ async function pushNewTag() {
 
   const tagName = `v${version}-extension`
   exec(
-    `git tag -a ${tagName} -m "version ${version}" && git push origin`,
+    `git commit -m "Release version ${version}" && git tag -a ${tagName} -m "version ${version}" && git push origin`,
     (error, stdout, stderr) => {
       if (error) {
-        console.error(`Failed to create or push git tag: ${error}`)
+        console.error(`Failed to commit, create, or push git tag: ${error}`)
         return
       }
       if (stderr) {
