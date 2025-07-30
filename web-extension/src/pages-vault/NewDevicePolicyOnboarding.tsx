48caa3f2d6465931b3806fee32ed51bd423f8ff3
Jiri Spac <capajj@gmail.com>
2025-07-30 08:07:22 +0200

    fix NewDevicePolicyOnboarding

--- web-extension/src/pages-vault/NewDevicePolicyOnboarding.tsx
+++ web-extension/src/pages-vault/NewDevicePolicyOnboarding.tsx
@@ -16,9 +16,8 @@ import {
   useUpdateNewDevicePolicyMutation,
   useGetUserNewDevicePolicyQuery
 } from './NewDevicePolicyOnboarding.codegen'
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { FiShield, FiShieldOff, FiAlertTriangle } from 'react-icons/fi'
+import { useState } from 'react'
+import { FiShieldOff, FiAlertTriangle } from 'react-icons/fi'
 import { Txt } from '@src/components/util/Txt'
 import { UserNewDevicePolicy } from '../../../shared/generated/graphqlBaseTypes'
 
@@ -27,12 +26,14 @@ export const NewDevicePolicyOnboarding = () => {
     useState<UserNewDevicePolicy | null>(null)
   const [updateNewDevicePolicy, { loading, error }] =
     useUpdateNewDevicePolicyMutation()
-  const navigate = useNavigate()
 
-  const { data: userData, loading: userLoading } =
-    useGetUserNewDevicePolicyQuery({
-      fetchPolicy: 'network-only'
-    })
+  const {
+    data: userData,
+    loading: userLoading,
+    refetch
+  } = useGetUserNewDevicePolicyQuery({
+    fetchPolicy: 'network-only'
+  })
 
   const handleSubmit = async () => {
     if (selectedPolicy) {
@@ -40,7 +41,7 @@ export const NewDevicePolicyOnboarding = () => {
         await updateNewDevicePolicy({
           variables: { newDevicePolicy: selectedPolicy }
         })
-        navigate('/')
+        refetch()
       } catch (e) {
         console.error(e)
       }
@@ -66,11 +67,18 @@ export const NewDevicePolicyOnboarding = () => {
       bg="rgba(0,0,0,0.5)"
       zIndex={9999}
     >
-      <Box p={8} borderWidth={1} borderRadius={8} boxShadow="lg" bg="white">
-        <Heading mb={6}>New Device Policy</Heading>
-        <Text mb={6}>Please select how you want to handle new devices.</Text>
+      <Box p={8} borderWidth={1} borderRadius={8} boxShadow="lg" bg="gray.50">
+        <Heading mb={6} color="gray.800">
+          New Device Policy
+        </Heading>
+        <Text mb={6} color="gray.700" fontSize={'md'}>
+          Please select how you want to handle new devices. You can change this
+          later in the settings.
+        </Text>
         <FormControl as="fieldset">
-          <FormLabel as="legend">Policy Options</FormLabel>
+          <FormLabel as="legend" color="gray.800">
+            Policy Options
+          </FormLabel>
           <RadioGroup
             onChange={(value) =>
               setSelectedPolicy(value as UserNewDevicePolicy)
@@ -78,34 +86,51 @@ export const NewDevicePolicyOnboarding = () => {
             value={selectedPolicy as string}
           >
             <Stack spacing={3}>
-              <Radio value={UserNewDevicePolicy.ALLOW}>
+              <Radio
+                value={UserNewDevicePolicy.ALLOW}
+                colorScheme="blue"
+                borderColor="gray.400"
+              >
                 <HStack spacing={2}>
                   <Icon as={FiShieldOff} color="orange.500" />
-                  <Text>Allow any new device</Text>
-                </HStack>
-              </Radio>
-              <Radio value={UserNewDevicePolicy.REQUIRE_ANY_DEVICE_APPROVAL}>
-                <HStack spacing={2}>
-                  <Icon as={FiAlertTriangle} color="yellow.500" />
-                  <Text>Require Any Device Approval</Text>
+                  <Text color="gray.800">Allow any new device</Text>
+                  <Text color="gray.500">
+                    You will be notified when a new device logged in.
+                  </Text>
                 </HStack>
               </Radio>
               <Radio
+                value={UserNewDevicePolicy.REQUIRE_ANY_DEVICE_APPROVAL}
+                colorScheme="blue"
+                borderColor="gray.400"
+              >
+                <HStack spacing={2}>
+                  <Icon as={FiAlertTriangle} color="yellow.500" />
+                  <Text color="gray.800">Require Any Device Approval</Text>
+                  <Text color="gray.500">
+                    You will be notified when a new device logged in and you
+                    need to approve it from another device.
+                  </Text>
+                </HStack>
+              </Radio>
+              {/* <Radio
                 disabled
                 value={UserNewDevicePolicy.REQUIRE_MASTER_DEVICE_APPROVAL}
+                colorScheme="gray"
+                borderColor="gray.300"
               >
                 <HStack spacing={2}>
                   <Icon as={FiShield} color="green.500" />
-                  <Text>Require Master Device Approval</Text>
+                  <Text color="gray.600">Require Master Device Approval</Text>
                 </HStack>
-              </Radio>
+              </Radio> */}
             </Stack>
           </RadioGroup>
         </FormControl>
-        <Txt mt={4} fontSize="sm" color="gray.500">
+        <Txt mt={4} fontSize="sm" color="gray.600">
           Note: The &quot;Require Master Device Approval&quot; option is
-          currently disabled. This feature will be available in a future update
-          when it is possible to reset your master device.
+          currently not available. This feature will be available in a future
+          update when it is possible to reset your master device.
         </Txt>
         {error && <Text color="red.500">{error.message}</Text>}
         <Button
