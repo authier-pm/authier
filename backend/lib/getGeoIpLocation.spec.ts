6f2941c6aae684569e62573fa09d2fc10d4b30fa
Jiri Spac <capajj@gmail.com>
2025-10-27 22:38:23 +0100

    bump deps

--- backend/lib/getGeoIpLocation.spec.ts
+++ backend/lib/getGeoIpLocation.spec.ts
@@ -1,15 +1,9 @@
 import { describe, expect, it } from 'vitest'
 import { getGeoIpLocation } from './getGeoIpLocation'
 
-describe(
-  'getGeoIpLocation',
-  async () => {
-    it('should return a location', async () => {
-      const res = await getGeoIpLocation.memoized('89.176.72.144')
-      expect(res.country_name).toEqual('Czechia')
-    })
-  },
-  {
-    timeout: 10000
-  }
-)
+describe('getGeoIpLocation', async () => {
+  it('should return a location', async () => {
+    const res = await getGeoIpLocation.memoized('89.176.72.144')
+    expect(res.country_name).toEqual('Czechia')
+  })
+}, 10000)
