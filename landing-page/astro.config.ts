ebc545458eabf2acfede0a20a3ba1057de17e77f
Jiri Spac <capajj@gmail.com>
2026-08-31 13:32:15 +0200

    fix: serve canonical page URLs without redirects

--- landing-page/astro.config.ts
+++ landing-page/astro.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   site: 'https://www.authier.pm',
+  build: {
+    format: 'file'
+  },
   integrations: [sitemap()],
   compressHTML: true,
   prefetch: true,
